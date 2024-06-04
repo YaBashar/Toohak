d4a83c352ec374b30e9104a4dd4634b8bf7d7e58
Amelia Su <z5478980@ad.unsw.edu.au>
2024-06-04 21:26:42 +1000

    complete stubbing for adminQuizList

--- src/quiz.js
+++ src/quiz.js
@@ -1,3 +1,24 @@
+// Function 6
+/** adminQuizList
+* <Gets all of the relevant information about the current quiz.>
+* @param {integer} authUserId - integer representing the user
+* ...
+* @returns {array} quizzes - an array containing all of the quizzes owned by the currently logged in user
+* @returns {integer} quizid - the numerical id for each quiz
+* @returns {string} name - the name of each quiz
+*/
+
+function adminQuizList(authUserId) {
+  return {
+    quizzes: [
+      {
+        quizId: 1,
+        name: 'My Quiz',
+      }
+    ]
+  }
+}
+
 function adminQuizCreate(authUserId, name, description) {
     quizId: 2
 }

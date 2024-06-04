6d36f4c7551999968b5821ab99c63c9013aa3aad
Amelia Su <z5478980@ad.unsw.edu.au>
2024-06-04 21:22:52 +1000

    first commit

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,16 @@
+// Function 4
+/** adminUserDetailsUpdate
+* <Gets all of the relevant information about the current quiz.>
+* @param {integer} authUserId - integer representing the user
+* @param {string} email
+* @param {string} nameFirst 
+* @param {string} nameLast
+*/
+function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
+  return {
+  }
+}
+
 // Function 5
 /** adminUserPasswordUpdate
 * <Gets all of the relevant information about the current quiz.>

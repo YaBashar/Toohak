fde5b0cb55e8edcf7210adb8e85c042ed0df9bb6
Parampreet Kaur <z5478214@ad.unsw.edu.au>
2024-06-04 18:27:11 +1000

    completed iter0/func1/auth.js/adminAuthRegister

---
+++ src/auth.js
@@ -0,0 +1,67 @@
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////   TOOHAK ITERATION 0 'AUTH.JS'  ////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+/*
+
+	COMP1531 24T2 --- Major Project: `Toohak', 
+	<https://nw-syd-gitlab.cseunsw.tech/COMP1531/24T2/groups/W11A_
+    CRUNCHIE/project-backend/-/blob/master/README.md>
+
+	This program was written by 
+    z5478214 | z5599894 | z5525050 | z5362173 | z5478980
+    on 04/06/2024
+
+	auth.js contains the stub functions for... 
+
+	
+*/
+
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////   GLOBAL DECLARATIONS    ////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+/*
+INCLUDED PACKAGES
+*/
+
+/*
+GLOBAL DEFINITIONS
+*/
+
+/*
+DATA STRUCTURES
+*/
+
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////   FUNCTION PROTOTYPES    ////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+// adminAuthRegister: [1]
+// adminAuthLogin: [2]
+// adminUserDetails: [3]
+// adminUserDetailsUpdate: [4]
+// adminUserPasswordUpdate: [5]
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////   FUNCTIONS   //////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+/** [1] adminAuthRegister
+  * 
+  * <Registers a user with an email, password, and name, 
+  *  then returns their authUserId value.>
+  * 
+  * @param {string} email - string containing user's email address
+  * @param {string} password - string containing user's password
+  * @param {string} nameFirst - string containing user's first name
+  * @param {string} nameLast - string containing user's last name
+  * ...
+  * 
+  * @returns {integer} - integer representing authUserId
+  * 
+*/
+
+function adminAuthRegister(email, password, nameFirst, nameLast) {
+    return {authUserId: 1};
+}

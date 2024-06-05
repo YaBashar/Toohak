eae6f0a6871194bd697b46435216e7db42f0b8ef
Sidak Harnoor Singh <z5525050@ad.unsw.edu.au>
2024-06-05 11:26:37 +1000

    made final changes to adminQuizRemove

--- src/quiz.js
+++ src/quiz.js
@@ -1,6 +1,18 @@
+/**
+ * 
+ * @param {integer} authUserId - integer representing the user
+ * @param {string} name - name of each quiz
+ * @param {string} description 
+ */
 function adminQuizCreate(authUserId, name, description) {
     quizId: 2
 }
+
+/**
+ * 
+ * @param {integer} authUserId -  integer representing the user
+ * @param {integer} quizId 
+ */
 function adminQuizRemove(authUserId, quizId) {
-    // empty object
+    // returns an empty object
 }

87dafddb9e198e08a993f2e7ed541010315f8e94
Sidak Harnoor Singh <z5525050@ad.unsw.edu.au>
2024-06-05 12:38:55 +1000

    made final changes to the function

--- src/quiz.js
+++ src/quiz.js
@@ -1,3 +1,12 @@
+/**
+ * 
+ * @param {integer} authUserId - integer representing the user
+ * @param {string} name - name of each quiz
+ * @param {string} description 
+ * @returns {integer}
+ */
 function adminQuizCreate(authUserId, name, description) {
-    quizId: 2
+    return {
+        quizId: 2
+    }
 }

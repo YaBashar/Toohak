4afa45d90b807c44fe1ac4ffa7a246212a35afef
Amelia Su <z5478980@ad.unsw.edu.au>
2024-06-10 20:35:12 +1000

    adminUserDetailsUpdate updated

--- src/adminUserDetailsUpdate.js
+++ src/adminUserDetailsUpdate.js
@@ -1,6 +1,6 @@
 // Function 4
 /** adminUserDetailsUpdate
-* <Gets all of the relevant information about the current quiz.>
+* <Update the properties of the logged in admin user>
 * @param {integer} authUserId - integer representing the user
 * @param {string} email
 * @param {string} nameFirst 
@@ -8,5 +8,39 @@
 */
 function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
   return {
+  }
+}
+
+// Function 5
+/** adminUserPasswordUpdate
+* <Update the password of the logged in user>
+* @param {integer} authUserId - integer representing the user
+* @param {string} oldPassword
+* @param {string} newPassword 
+*/
+function adminUserPasswordUpdate(authUserId, oldPassword, newPassword) {
+  return {
+  }
+}
+
+
+// Function 6
+/** adminQuizList
+* <Gets all of the relevant information about the current quiz.>
+* @param {integer} authUserId - integer representing the user
+* ...
+* @returns {} quizzes - an array containing all of the quizzes owned by the currently logged in user
+* @returns {integer} quizid - the numerical id for each quiz
+* @returns {string} name - the name of each quiz
+*/
+
+function adminQuizList(authUserId) {
+  return {
+    quizzes: [
+      {
+        quizId: 1,
+        name: 'My Quiz',
+      }
+    ]
   }
 }

460c84095efb08dc6d339c643b57f84cd97d92b6
Amelia Su <z5478980@ad.unsw.edu.au>
2024-06-17 09:34:02 +1000

    completed tests for adminQuizList

---
+++ tests/adminQuizList.test.js
@@ -0,0 +1,17 @@
+import { adminQuizList } from './auth.js'
+import { clear } from './other.js'
+
+beforeEach(() => {
+  clear();
+});
+
+describe('Testing for errors', () => {
+  // AuthUserId isn't valid
+  test('Invalid AuthUserId', () => {
+    const result1 = adminQuizList('randomstring');
+    expect(result1).toStrictEqual({error: expect.any(String)});
+
+    const result2 = adminQuizList(1);
+    expect(result2).toStrictEqual({error: expect.any(String)});
+  });
+});

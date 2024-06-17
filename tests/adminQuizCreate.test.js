5123cf3f1ac931effab68cca339a0845acf8d624
Sidak Harnoor Singh <z5525050@ad.unsw.edu.au>
2024-06-17 19:14:30 +1000

    tests for QuizRemoe function

---
+++ tests/adminQuizCreate.test.js
@@ -0,0 +1,44 @@
+import { adminQuizCreate } from "../src/quiz";
+import { adminAuthRegister } from "../src/auth";
+import { clear } from "../src/other";
+
+let authUserId;
+
+beforeEach(() => {
+  clear();
+  authUserId = adminAuthRegister('z5525050@unsw.edu.au', '123ABCabc@#$', 'sidak', 'singh');
+});
+
+describe('Testing for errors', () => {
+  test('AuthUserId is not a valid user', () => {
+    //
+    const invalidUserId = 'invalidUserId';
+    const result = adminQuizCreate(invalidUserId, 'name', 'description')
+    expect(result).toStrictEqual({ error: 'Invalid user id'})
+  });
+  test('Name contains invalid characters', () => {
+    const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '=', '{', '}', '[', ']', 
+                          ':', ';', '-', '"', "'", '<', '>', '.', '?', '/', '|', '\\'];  
+    const result = adminQuizCreate(authUserId, 'sidak', 'valid description');
+    expect(result).toStrictEqual({ error: expect.any(String) })
+  });
+
+  test('Name is too short', () => {
+    const result = adminQuizCreate(authUserId, 's', 'valid description')
+    expect(result).toStrictEqual({error : 'name is less than 3 characters'})
+  });
+  test('Name is too long', () => {
+    const result = adminQuizCreate(authUserId, 'abcdefghijklmnopqrstuvwxyzabcde', 'valid description')
+    expect(result).toStrictEqual({error : 'name is more than 30 characters'})
+  });
+  test('Name is already used by current user', () => {
+    // Name is already used by the current logged in user for another quiz.
+    
+  });
+  test('Description is more than 100 characters', () => {
+    //  Description is more than 100 characters in length (note: empty strings are OK)
+    const longDescription = 'a'.repeat(101);
+    const result = adminQuizCreate(authUserId, 'Sidak', longDescription);
+    expect(result).toStrictEqual({error: 'Description is more than 100 characters in length'})
+  });
+});
---
+++ tests/adminQuizRemove.test.js
@@ -0,0 +1,30 @@
+import { adminQuizCreate, adminQuizRemove } from "../src/quiz";
+import { adminAuthRegister } from "../src/auth";
+import { clear } from "../src/other";
+
+let authUserId;
+
+beforeEach(() => {
+	clear();
+	authUserId = adminAuthRegister('z5525050@unsw.edu.au', '123ABCabc@#$', 'sidak', 'singh');
+	validQuizId = adminQuizCreate(authUserId, 'validQuiz', 'valid description').quizId;
+	otherUserId = adminAuthRegister('otherUser', 'otherPassword');
+});
+
+describe('Testing for errors', () => {
+	test('AuthUserId is not a valid user', () => {
+		const invalidUserId = 'invalidUserId';
+		const result = adminQuizRemove(invalidUserId, validQuizId)
+		expect(result).toStrictEqual({ error: 'AuthUserId is not a valid user' }) 
+	});
+	test('Quiz Id does not refer to a valid quiz', () => {
+		const invalidQuizId = 'invalidQuizId';
+		const result = adminQuizRemove(authUserId, invalidQuizId)
+		expect(result).toStrictEqual({ error: 'Invalid quiz Id entered '})
+	});
+	test('Quiz ID does not refer to a quiz that this user owns ', () => {
+		const result = adminQuizRemove(otherUserId, validQuizId)
+		expect(result).toStrictEqual({ error: 'Quiz Id not owned by the user '})
+	});
+});
+

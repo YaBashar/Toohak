225c9b3f454a5e3df90568a8a8e023847ae987f1
Sidak Harnoor Singh <z5525050@ad.unsw.edu.au>
2024-06-18 23:01:54 +1000

    Finished final implementation of adminQuizRemove function and all tests passed successfully

--- tests/adminQuizCreate.test.js
+++ tests/adminQuizCreate.test.js
@@ -6,23 +6,20 @@ let authUserId;
 
 beforeEach(() => {
   clear();
-  authUserId = adminAuthRegister('z5525050@unsw.edu.au', '123ABCabc@#$', 'sidak', 'singh');
 });
 
-describe('Testing for errors', () => {
-  test('AuthUserId is not a valid user', () => {
-    //
-    const invalidUserId = 'invalidUserId';
-    const result = adminQuizCreate(invalidUserId, 'name', 'description')
-    expect(result).toStrictEqual({ error: 'Invalid user id'})
-  });
+describe('Testing for adminQuizCreate', () => {
+  const authUserId = adminAuthRegister('z5525050@unsw.edu.au', '123ABCabc@#$', 'sidak', 'singh').authUserId;
+  test("AuthUserId is invalid", () => {
+    const result = adminQuizCreate('invalidAuthUserId', 'Sidak', 'valid description');
+    expect(result).toStrictEqual({ error: 'Invalid User id' });
+  })
   test('Name contains invalid characters', () => {
     const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '=', '{', '}', '[', ']', 
                           ':', ';', '-', '"', "'", '<', '>', '.', '?', '/', '|', '\\'];  
-    const result = adminQuizCreate(authUserId, 'sidak', 'valid description');
+    const result = adminQuizCreate(authUserId, 'sid!ak', 'valid description');
     expect(result).toStrictEqual({ error: expect.any(String) })
   });
-
   test('Name is too short', () => {
     const result = adminQuizCreate(authUserId, 's', 'valid description')
     expect(result).toStrictEqual({error : 'name is less than 3 characters'})
@@ -31,14 +28,23 @@ describe('Testing for errors', () => {
     const result = adminQuizCreate(authUserId, 'abcdefghijklmnopqrstuvwxyzabcde', 'valid description')
     expect(result).toStrictEqual({error : 'name is more than 30 characters'})
   });
-  test('Name is already used by current user', () => {
-    // Name is already used by the current logged in user for another quiz.
-    
+  test('Name is already used by current logged in user', () => {
+    adminQuizCreate(authUserId, 'Sidak', 'valid description');
+    const result = adminQuizCreate(authUserId, 'Sidak', 'description');
+    expect(result).toStrictEqual( {error: expect.any(String)} );
   });
   test('Description is more than 100 characters', () => {
-    //  Description is more than 100 characters in length (note: empty strings are OK)
     const longDescription = 'a'.repeat(101);
     const result = adminQuizCreate(authUserId, 'Sidak', longDescription);
     expect(result).toStrictEqual({error: 'Description is more than 100 characters in length'})
   });
+  test('Quiz created successfully', () => {
+    const result = adminQuizCreate(authUserId, 'john', 'toohak quiz');
+    //quiz created successfully
+    expect(result).toStrictEqual({ quizId: expect.any(Number) });
+  });
 });
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
--- tests/adminQuizRemove.test.js
+++ tests/adminQuizRemove.test.js
@@ -2,29 +2,35 @@ import { adminQuizCreate, adminQuizRemove } from "../src/quiz.js";
 import { adminAuthRegister } from "../src/auth.js";
 import { clear } from "../src/other.js";
 
-let authUserId;
-
 beforeEach(() => {
 	clear();
-	authUserId = adminAuthRegister('z5525050@unsw.edu.au', '123ABCabc@#$', 'sidak', 'singh');
-	validQuizId = adminQuizCreate(authUserId, 'validQuiz', 'valid description').quizId;
-	otherUserId = adminAuthRegister('otherUser', 'otherPassword');
 });
 
-describe('Testing for errors', () => {
-	test('AuthUserId is not a valid user', () => {
-		const invalidUserId = 'invalidUserId';
-		const result = adminQuizRemove(invalidUserId, validQuizId)
-		expect(result).toStrictEqual({ error: 'AuthUserId is not a valid user' }) 
+describe('Testing for adminQuizRemove function', () => {
+	const authUserId = adminAuthRegister('z5525050@unsw.edu.au', '123ABCabc@#$', 'sidak', 'singh').authUserId;
+	const validQuizId = adminQuizCreate(authUserId, 'validQuiz', 'valid description').quizId;
+	const otherUserId = adminAuthRegister('z5555555@unsw.edu.au', 'abs@#$234', 'brim', 'Johnson').authUserId;
+
+	// test to check if the authUserId is invalid
+	test("AuthUserId is invalid", () => {
+		const result = adminQuizCreate('invalidAuthUserId', 'Sidak', 'valid description');
+		expect(result).toStrictEqual({ error: 'Invalid User id' });
 	});
+	// test to check quiz Id does not refer to a valid quiz
 	test('Quiz Id does not refer to a valid quiz', () => {
 		const invalidQuizId = 'invalidQuizId';
 		const result = adminQuizRemove(authUserId, invalidQuizId)
-		expect(result).toStrictEqual({ error: 'Invalid quiz Id entered '})
+		expect(result).toStrictEqual({ error: 'Invalid quiz Id entered'})
 	});
+	// test to check if quiz ID does not refer to a quiz that this user owns
 	test('Quiz ID does not refer to a quiz that this user owns ', () => {
 		const result = adminQuizRemove(otherUserId, validQuizId)
-		expect(result).toStrictEqual({ error: 'Quiz Id not owned by the user '})
+		expect(result).toStrictEqual({ error: 'Quiz Id not owned by the user'})
+	});
+	// write a test to check if the quiz is removed from the list of quizzes
+	test('Quiz is removed from the list of quizzes', () => {
+		const result = adminQuizRemove(authUserId, validQuizId);
+		expect(result).toStrictEqual({});
 	});
 });
 

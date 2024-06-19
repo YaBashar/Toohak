fff54dcca770e65225b819a351b7894fa1ac3636
Sidak Harnoor Singh <z5525050@ad.unsw.edu.au>
2024-06-19 13:27:08 +1000

    just a style change

--- tests/adminQuizCreate.test.js
+++ tests/adminQuizCreate.test.js
@@ -2,7 +2,6 @@ import { adminQuizCreate } from "../src/quiz.js";
 import { adminAuthRegister } from "../src/auth.js";
 import { clear } from "../src/other.js";
 
-
 beforeEach(() => {
   clear();
 });

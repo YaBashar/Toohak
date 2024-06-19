117ac99c8109f361439dad9461b4f7a6d0965766
Amelia Su <z5478980@ad.unsw.edu.au>
2024-06-19 13:12:43 +1000

    included an extra quiz in the successful test case

--- tests/adminQuizList.test.js
+++ tests/adminQuizList.test.js
@@ -4,21 +4,23 @@ import { clear } from '../src/other.js'
 
 beforeEach(() => {
   clear();
-  let id = adminAuthRegister('amelia@unsw.edu.au', 'ABCDabcd1234!@#$', 'Amelia', 'Su').authId;
+  let id = adminAuthRegister('amelia@unsw.edu.au', 'ABCDabcd1234!@#$', 'Amelia', 'Su').authUserId;
+  console.log(id);
   let quiz = adminQuizCreate(id, 'quiz 1', 'the first quiz');
+  let quiz2 = adminQuizCreate(id, 'quiz 1', 'the first quiz');
 });
 
 describe('Testing for errors', () => {
   // AuthUserId isn't valid
   test('Invalid AuthUserId', () => {
     const result1 = adminQuizList('randomstring');
-    expect(result1).toStrictEqual({error: expect.any(String)});
+    expect(result1).toStrictEqual({error: 'invalid user id'});
 
     const result2 = adminQuizList('1');
-    expect(result2).toStrictEqual({error: expect.any(String)});
+    expect(result2).toStrictEqual({error: 'invalid user id'});
   });
 
-  test('Expected results', () => {
+  test.only('Expected results', () => {
     const result3 = adminQuizList(id);
     expect(result3).toStrictEqual(
       { quizzes: [

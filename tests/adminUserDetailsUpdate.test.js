f425fd03314194c4ee088ea9033879cd8209153a
Amelia Su <z5478980@ad.unsw.edu.au>
2024-06-19 12:49:44 +1000

    changed labelling for id in before each

--- tests/adminUserDetailsUpdate.test.js
+++ tests/adminUserDetailsUpdate.test.js
@@ -1,11 +1,14 @@
 import { adminUserDetailsUpdate, adminAuthRegister } from '../src/auth.js'
 import { clear } from '../src/other.js'
 
-let authUserId;
+let id;
+let user;
 
 beforeEach(() => {
   clear();
-  authUserId = adminAuthRegister('amelia@unsw.edu.au', 'abcd1234!@#$ABCD', 'amelia', 'su').authUserId;
+  user = adminAuthRegister('amelia@unsw.edu.au', 'abcd1234!@#$ABCD', 'amelia', 'su');
+  id = user.authUserId;
+  console.log(id);
 });
 
 describe('Testing adminUserDetailsUpdate for errors', () => {
@@ -16,52 +19,52 @@ describe('Testing adminUserDetailsUpdate for errors', () => {
 
   // Email is currently used by another user (excluding the current authorised user)
   test('Email is already used by another user', () => {
-    const authUserId2 = adminAuthRegister('steph@unsw.edu.au', 'Farmingsimulator!1234', 'steph', 'liang');
-    const result = adminUserDetailsUpdate (authUserId2.authUserId, 'amelia@unsw.ed.au', 'steph', 'liang');
+    const authUserId2 = adminAuthRegister('steph@unsw.edu.au', 'Farmingsimulator!1234', 'steph', 'liang').authUserId;
+    const result = adminUserDetailsUpdate (authUserId2, 'amelia@unsw.edu.au', 'steph', 'liang');
     expect(result).toStrictEqual({ error: 'email used by another user' });
   });
 
   // Email is not valid
   test('Email is not a valid email', () => {
-    const result = adminUserDetailsUpdate(authUserId, 'gurigiurabgiurag', 'amelia', 'su');
+    const result = adminUserDetailsUpdate(id, 'gurigiurabgiurag', 'amelia', 'su');
     expect(result).toStrictEqual({ error: 'invalid email address' })
   });
 
   // First/last name contains invalid characters
   test('First name contains invalid characters', () => {
-    const result = adminUserDetailsUpdate(authUserId, 'amelia@unsw.ed.au', 'a!melia', 'su');
+    const result = adminUserDetailsUpdate(id, 'amelia@unsw.ed.au', 'a!melia', 'su');
     expect(result).toStrictEqual({ error: 'first name contains invalid characters' })
   });
 
   test('Last name contains invalid characters', () => {
-    const result = adminUserDetailsUpdate(authUserId, 'amelia@unsw.ed.au', 'amelia', 'su+');
+    const result = adminUserDetailsUpdate(id, 'amelia@unsw.ed.au', 'amelia', 'su+');
     expect(result).toStrictEqual({ error: 'last name contains invalid characters' });
   });
 
   // First/last name is too short or too long
   test('First name is too short', () => {
-    const result = adminUserDetailsUpdate(authUserId, 'amelia@unsw.ed.au', 'a', 'su');
+    const result = adminUserDetailsUpdate(id, 'amelia@unsw.ed.au', 'a', 'su');
     expect(result).toStrictEqual({ error: 'first name is too short' });
   });
 
   test('First name is too long', () => {
-    const result = adminUserDetailsUpdate(authUserId, 'amelia@unsw.ed.au', 'abcdefghijklmnopqrstuv', 'su');
+    const result = adminUserDetailsUpdate(id, 'amelia@unsw.ed.au', 'abcdefghijklmnopqrstuv', 'su');
     expect(result).toStrictEqual({ error: 'first name is too long'});
   });
 
   test('Last name is too short', () => {
-    const result = adminUserDetailsUpdate(authUserId, 'amelia@unsw.ed.au', 'amelia', 's');
+    const result = adminUserDetailsUpdate(id, 'amelia@unsw.ed.au', 'amelia', 's');
     expect(result).toStrictEqual({ error: 'last name is too short' });
   });
 
   test('Last name is too long', () => {
-    const result = adminUserDetailsUpdate(authUserId, 'amelia@unw.ed.au', 'amelia', 'abcdefghijklmnopqrstuv');
+    const result = adminUserDetailsUpdate(id, 'amelia@unw.ed.au', 'amelia', 'abcdefghijklmnopqrstuv');
     expect(result).toStrictEqual({ error: 'last name is too long' });
   });
 
   // successful use of function
   test('function used correctly', () => {
-    const result = adminUserDetailsUpdate(authUserId, 'amelia1@unsw.edu.au', 'amelia', 'su');
+    const result = adminUserDetailsUpdate(id, 'amelia1@unsw.edu.au', 'amelia', 'su');
     expect(result).toStrictEqual({});
   });
 });

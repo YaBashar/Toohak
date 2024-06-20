50d292b40a46db25d5566d6df7d86a0e9fd9fcc3
Sidak Harnoor Singh <z5525050@ad.unsw.edu.au>
2024-06-20 13:31:37 +1000

    made changes to the test file

--- tests/adminQuizCreate.test.js
+++ tests/adminQuizCreate.test.js
@@ -7,44 +7,55 @@ beforeEach(() => {
 });
 
 describe('Testing for adminQuizCreate', () => {
-  const authUserId = adminAuthRegister('z5525050@unsw.edu.au', '123ABCabc@#$', 'sidak', 'singh').authUserId;
+
+  let id;
+  beforeEach(() => {
+    id = adminAuthRegister('z5525050@unsw.edu.au', '123ABCabc@#$', 'sidak', 'singh');
+  });
+
+ 
   test("AuthUserId is invalid", () => {
     const result = adminQuizCreate('invalidAuthUserId', 'Sidak', 'valid description');
     expect(result).toStrictEqual({ error: 'Invalid User id' });
   })
+
   test('Name contains invalid characters', () => {
     const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '=', '{', '}', '[', ']', 
                           ':', ';', '-', '"', "'", '<', '>', '.', '?', '/', '|', '\\'];  
-    const result = adminQuizCreate(authUserId, 'sid!ak', 'valid description');
-    expect(result).toStrictEqual({ error: expect.any(String) })
+    const result = adminQuizCreate(id.authUserId, 'sid!ak', 'valid description');
+    expect(result).toStrictEqual({ error: expect.any(String) });
   });
+
   test('Name is too short', () => {
-    const result = adminQuizCreate(authUserId, 's', 'valid description')
-    expect(result).toStrictEqual({error : 'name is less than 3 characters'})
+    const result = adminQuizCreate(id.authUserId, 's', 'valid description');
+    expect(result).toStrictEqual({error : 'name is less than 3 characters'});
   });
+
   test('Name is too long', () => {
-    const result = adminQuizCreate(authUserId, 'abcdefghijklmnopqrstuvwxyzabcde', 'valid description')
-    expect(result).toStrictEqual({error : 'name is more than 30 characters'})
+    const result = adminQuizCreate(id.authUserId, 'abcdefghijklmnopqrstuvwxyzabcde', 'valid description');
+    expect(result).toStrictEqual({error : 'name is more than 30 characters'});
   });
+
   test('Name is already used by current logged in user', () => {
-    adminQuizCreate(authUserId, 'Sidak', 'valid description');
-    const result = adminQuizCreate(authUserId, 'Sidak', 'description');
+    adminQuizCreate(id.authUserId, 'Sidak', 'valid description');
+    const result = adminQuizCreate(id.authUserId, 'Sidak', 'description');
     expect(result).toStrictEqual( {error: expect.any(String)} );
   });
+
   test('Description is more than 100 characters', () => {
     const longDescription = 'a'.repeat(101);
-    const result = adminQuizCreate(authUserId, 'Sidak', longDescription);
-    expect(result).toStrictEqual({error: 'Description is more than 100 characters in length'})
+    const result = adminQuizCreate(id.authUserId, 'Sidak', longDescription);
+    expect(result).toStrictEqual({error: 'Description is more than 100 characters in length'});
   });
+
   test('Quiz created successfully', () => {
-    const result = adminQuizCreate(authUserId, 'john', 'toohak quiz');
+    const result = adminQuizCreate(id.authUserId, 'john', 'toohak quiz');
     //quiz created successfully
     expect(result).toStrictEqual({ quizId: expect.any(Number) });
   });
+
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 

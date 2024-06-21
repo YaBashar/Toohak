ed12123d8c16edb0971176ac0ed335632a9f8feb
Parampreet Kaur <z5478214@ad.unsw.edu.au>
2024-06-21 13:02:11 +1000

    made expected returns specific in adminAuthLogin.test.js

--- tests/adminAuthLogin.test.js
+++ tests/adminAuthLogin.test.js
@@ -10,14 +10,14 @@ describe('Testing login', () => {
   // Email address does not exist.
   test('Email address does not exist', () => {
     const result = adminAuthLogin('zid@unsw.edu.au', 'abcd1234');
-    expect(result).toStrictEqual({error: expect.any(String)});
+    expect(result).toStrictEqual({error: 'Email address does not exist'});
   });
 
   // Password is not correct for the given email.
   test('Incorrect password', () => {
     adminAuthRegister('zid@unsw.edu.au', 'abcd1234', 'first', 'last');
     const result = adminAuthLogin('zid@unsw.edu.au', '1234abcd');
-    expect(result).toStrictEqual({error: expect.any(String)});
+    expect(result).toStrictEqual({error: 'Incorrect password'});
   });
 
   // Successful Login

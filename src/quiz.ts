43e0faea84fc97a8dc11a59f6bfb5c7d102b047b
Amelia Su <z5478980@ad.unsw.edu.au>
2024-06-27 11:04:38 +1000

    adminQuizList changed from js to ts and let changed to const

---
+++ src/quiz.ts
@@ -0,0 +1,330 @@
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////   TOOHAK ITERATION 0 'QUIZ.JS'  ////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+/*
+
+	COMP1531 24T2 --- Major Project: `Toohak', 
+	<https://nw-syd-gitlab.cseunsw.tech/COMP1531/24T2/groups/W11A_
+  CRUNCHIE/project-backend/-/blob/master/README.md>
+
+	This program was written by 
+  z5478214 | z5599894 | z5525050 | z5362173 | z5478980
+  on 04/06/2024
+
+	quiz.js contains the stub functions for the implementation of quiz mechanics
+  in the Toohak project. This includes functions that create, remove, list
+  and update information regarding quizzes. 
+	
+*/
+
+
+///////////////////////////////////////////////////////////////////////////////
+/////////////////////////   GLOBAL DECLARATIONS    ////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+/*
+DEPENDENCIES
+*/
+import { getData, setData } from "./dataStore.js";
+
+/*
+GLOBAL DEFINITIONS
+*/
+
+/*
+DATA STRUCTURES
+*/
+
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////   FUNCTION CONTENTS    /////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+// adminQuizList: [1]
+// adminQuizCreate: [2]
+// adminQuizRemove: [3]
+// adminQuizInfo: [4]
+// adminQuizNameUpdate: [5]
+// adminQuizDescriptionUpdate: [6]
+
+ 
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////   FUNCTIONS   //////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+/** [1] adminQuizList
+  * 
+  * Provides a list of all quizzes that are owned by the currently 
+  * logged in user.
+  * 
+  * @param {number} authUserId - number representing a unique 
+  *                              identifier for the user
+  * ...
+  * @returns {
+  *   quizzes : [
+  *     {
+  *      quizId: number,
+  *      name: string,
+  *     }
+  *   ]
+  * } - an array containing the names of all quizzes and their quizIds
+  * 
+*/
+export function adminQuizList(authUserId: String): Object {
+  const data = getData();
+  const user = data.users.find(user => user.authUserId === authUserId)
+  const allQuizzes = [];
+
+  if (!Number.isInteger(authUserId)) {
+    return { error: 'invalid user id' };
+  };
+
+  const userQuizzes = data.quizzes
+    .filter(quiz => quiz.authUserId === authUserId)
+    .map(quiz => ({
+      quizId: quiz.quizId,
+      name: quiz.name,
+    }));
+
+  return { quizzes: userQuizzes };
+}
+
+/** [2] adminQuizCreate
+  * 
+  * Given basic details about a new quiz, create one for the logged in user.
+  * 
+  * @param {number} authUserId - number representing a unique 
+  *                              identifier for the user 
+  * @param {number} quizId - number representing a unique 
+  *                          identifier for the quiz
+  * @param {string} description - string containing description of the quiz
+  * ...
+  * @returns {quizId: number} - number representing a unique 
+  *                             identifier for the quiz 
+  * 
+*/
+export function adminQuizCreate(authUserId, name, description) {
+  let store = getData();
+  let userArr = store.users;
+  let quizArr = store.quizzes;
+  const user = userArr.find((user) => user.authUserId === authUserId);
+  if (!user) return {error: 'Invalid User id'};
+  
+  const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '=', '{', '}', '[', ']', 
+                        ':', ';', '-', '"', "'", '<', '>', '.', '?', '/', '|', '\\'];
+  for (let i = 0; i < specialChars.length; i++) {
+    if (name.includes(specialChars[i])) {
+      return { error: 'Name contains invalid characters' };
+    }
+  }
+  if (name.length < 3) {
+    return { error: 'name is less than 3 characters' };
+  }
+  if (name.length > 30) {
+    return { error: 'name is more than 30 characters' };
+  }
+  if (description.length > 100) {
+    return { error: 'Description is more than 100 characters in length' };
+  }
+  if (quizArr.find((quiz) => quiz.name === name && quiz.authUserId === authUserId)) {
+    return { error: 'Name is already used by current logged in user' };
+  }
+  
+  const id = quizArr.length + 1;
+  const quiz = {
+    quizId: id,
+    name: name,
+    description: description,
+    authUserId: authUserId,
+  };
+  store.quizzes.push(quiz);
+  setData(store);
+  return { quizId: id };
+}
+
+
+
+/** [3] adminQuizRemove
+  * 
+  * Given a particular quiz, permanently remove the quiz.
+  * 
+  * @param {number} authUserId - number representing a unique 
+  *                              identifier for the user  
+  * @param {number} quizId - number representing a unique 
+  *                          identifier for the quiz  
+  * ...
+  * @returns {} - empty object
+  * 
+*/
+
+export function adminQuizRemove(authUserId, quizId) {
+  let store = getData();
+  let quizArray = store.quizzes;
+  let userArray = store.users;
+  let user = userArray.find(user => user.authUserId === authUserId);
+  let quiz = quizArray.find(quiz => quiz.quizId === quizId);
+  if (!user) {
+    return { error: 'Invalid user id' };
+  }
+  if (!quiz) {
+    return { error: 'Invalid quiz Id entered' };
+  }
+  if (quiz.authUserId !== authUserId) {
+    return { error: 'Quiz Id not owned by the user' };
+  }
+  let index = quizArray.indexOf(quiz);
+  quizArray.splice(index, 1);
+  store.quizzes = quizArray;
+  setData(store);
+  return {};
+}
+
+
+
+/** [4] adminQuizInfo
+  * 
+  * Gets all of the relevant information about the current quiz.
+  * 
+  * @param {number} authUserId - number representing a unique 
+  *                              identifier for the user   
+  * @param {number} quizId - number representing a unique 
+  *                          identifier for the quiz 
+  * ...
+  * @returns {
+  *   quizId: number,
+  *   name: string,
+  *   timeCreated: number,
+  *   timeLastEdited: number,
+  *   description: string,
+  * } - an object with information about the quiz based on the quizId
+  * 
+*/
+
+export function adminQuizInfo(authUserId, quizId) {
+  let store = getData();
+  let userArr = store.users;
+  let quizArr = store.quizzes;
+
+  const quiz = quizArr.find((quiz) => quiz.quizId === quizId);
+  const user = userArr.find((user) => user.authUserId === authUserId);
+  const userQuiz = quizArr.find((quiz) => quiz.authUserId === authUserId);
+
+  if (!quiz ) {
+    return {error: "Invalid Quiz id"};
+  } else if (!user) {
+    return {error: "Invalid User id"};
+  } else if (!userQuiz) {
+    return {error : "This Quiz Id does not refer to a quiz that this user owns"}
+  }
+
+  return {
+    quizId: quizId,
+    name: quiz.name,
+    timeCreated: 1683125870,
+    timeLastEdited: 1683125871,
+    description: quiz.description
+  };
+}
+
+
+
+
+/** [5] adminQuizNameUpdate
+  * 
+  * Update the name of the relevant quiz.
+  * 
+  * @param {number} authUserId - number representing a unique 
+  *                              identifier for the user 
+  * @param {number} quizId - number representing a unique 
+  *                          identifier for the quiz 
+  * @param {string} name - string containing the current name of the quiz
+  * ...
+  * @returns {} - empty object
+  * 
+*/
+
+export function adminQuizNameUpdate(authUserId, quizId, name) {
+  return {
+  };
+}
+
+/** [6] adminQuizDescriptionUpdate
+  * 
+  * Update The description of the relevant quiz.
+  * 
+  * @param {number} authUserId - number representing a unique 
+  *                              identifier for the user
+  * @param {string} description - a string containing the current
+  *                               description of the quiz
+  * ...
+  * @returns {} - empty object
+  * 
+*/
+
+/** [11] adminQuizDescriptionUpdate
+  * 
+  * Update The description of the relevant quiz.
+  * 
+  * @param {number} authUserId - Id number representing a unique 
+  *                              identifier for the user
+  * @param {number} quizId     - Id number representing a unique
+  *                              identifier for the quiz
+  * @param {string} description - a string containing the current
+  *                               description of the quiz
+  * ...
+  * @returns {} - empty object if successful
+  * 
+*/
+
+// My constant define for the 'Description is more than 100 characters' test case
+const MAX_DESCRIPTION_LENGTH = 100;
+
+export function adminQuizDescriptionUpdate(authUserId, quizId, description) {
+  let store = getData();
+  let userArr = store.users;
+  let quizArr = store.quizzes;
+
+  // Type checks for authUserId and quizId
+  if (typeof authUserId !== 'number') {
+    return { error: 'Invalid user Id type' };
+  }
+  
+  if (typeof quizId !== 'number') {
+    return { error: 'Invalid quiz Id type' };
+  }
+  
+  // These two lines finds the Tahook user with both a valid userId and quidId
+  const user = userArr.find((user) => user.authUserId === authUserId);
+  const quiz = quizArr.find((quiz) => quiz.quizId == quizId);
+
+  // Check if the quiz is owned by the user with the given UserId
+  const quizUser = quizArr.find((quiz) => quiz.authUserId == authUserId);
+
+  // Error messages returned if the error tests cases are activated within the program
+  // If a person's Tahook quiz does not match the userId, an error will then be returned
+  if (!quizUser) {
+    return { error: 'Quiz Id not owned by the user' };
+  }
+
+  // If the description length exceeds 100 characters, return an error
+  if (description.length > MAX_DESCRIPTION_LENGTH) {
+    return { error: "Quiz description is more than 100 characters in length" };
+  }
+
+  // If the Tahook user does not exist, an error will then be returned
+  if (!user) {
+    return {error: 'authUserId does not exist'};
+
+   // If a person's Tahook quiz does not exist, an error will then be returned
+  } else if (!quiz) {
+    return {error: 'quizId does not exist'};
+  
+  } else {
+
+    quiz.description = description;
+    setData(store);
+
+    return {};
+
+  }
+}

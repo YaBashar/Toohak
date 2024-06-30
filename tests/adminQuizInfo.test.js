80945214a6c8710925ce4f0952a56f0dc46e6fb5
MUBASHIR MUHAMMAD HUSSAIN <z5599894@ad.unsw.edu.au>
2024-06-30 16:47:31 +1000

    Completed conversion of tests in adminQuizInfo.test.js to server tests

--- tests/adminQuizInfo.test.js
+++ tests/adminQuizInfo.test.js
@@ -1,18 +1,15 @@
-import { adminAuthRegister } from '../src/auth.js';
-import { adminQuizInfo, adminQuizCreate, adminQuizNameUpdate } from '../src/quiz.js';
-import { clear } from '../src/other.js';
-
 import request from 'sync-request-curl';
 import { port, url } from './config.json';
 
 const SERVER_URL = `${url}:${port}`;
 
 // Helper Functions for requests
+/// /////////////////////////////////////////////////////////////
 const createUser = (email, password, nameFirst, nameLast) => {
   const res = request(
     'POST',
-    SERVER_URL + '/admin/auth/register',
-    { json: { email: email, password: password, nameFirst: nameFirst, nameLast: nameLast } });
+    SERVER_URL + 'v1/admin/auth/register',
+    { json: { email, password, nameFirst, nameLast } });
   return JSON.parse(res.body.toString());
 };
 
@@ -20,7 +17,7 @@ const createQuiz = (token, name, description) => {
   const res = request(
     'POST',
     SERVER_URL + 'v1/admin/quiz',
-    { json: { authUserId: token, name: name, description: description } });
+    { json: { token, name, description } });
   return JSON.parse(res.body.toString());
 };
 
@@ -28,10 +25,11 @@ const quizNameUpdate = (token, quizId, name) => {
   const res = request(
     'PUT',
     SERVER_URL + 'v1/admin/quiz/:quizid/name',
-    { json: { authUserId: token, quizId: quizId, name: name } }
+    { json: { token, quizId, name } }
   );
   return JSON.parse(res.body.toString());
 };
+/// /////////////////////////////////////////////////////////////
 
 beforeEach(() => {
   request('DELETE', SERVER_URL + 'v1/clear');
@@ -39,41 +37,43 @@ beforeEach(() => {
 
 describe('adminQuizInfo Tests', () => {
   describe('Error Cases', () => {
-    let authUserId;
+    let token;
     let quizId;
 
     beforeEach(() => {
-      authUserId = createUser('hayden@gmail.com', '1password', 'Hayden', 'Smith').authUserId;
-      quizId = createQuiz(createUser.authUserId, 'quizName', 'description').quizId;
+      token = createUser('hayden@gmail.com', '1password', 'Hayden', 'Smith').token;
+      quizId = createQuiz(token, 'quizName', 'description').quizId;
     });
 
     test('Info of a Quiz which does not exist ', () => {
-      const quizInfo = request('GET', SERVER_URL + `v1/admin/quiz/${quizId + 1}`, { qs: { authUserId } });
+      const quizInfo = request('GET', SERVER_URL + `v1/admin/quiz/${quizId + 1}`, { qs: { token } });
       expect(JSON.parse(quizInfo.body.toString())).toStrictEqual({ error: expect.any(String) });
     });
 
     test('Info of a Quiz with invalid Authuser id', () => {
-      // const quizInfo = request('GET', SERVER_URL + `/admin/quiz/${quizId}`, { qs: { authUserId } });
-      // expect(JSON.parse(quizInfo.body.toString())).toStrictEqual({ error: expect.any(String) });
+      const quizInfo = request('GET', SERVER_URL + `v1/admin/quiz/${quizId}`, { qs: { token: token + 1 } });
+      expect(JSON.parse(quizInfo.body.toString())).toStrictEqual({ error: expect.any(String) });
     });
 
     test('Quiz Id does not refer to a quiz that this user owns', () => {
-      // const result = adminQuizInfo(authUserId + 1, quizId, 'Any description');
-      // expect(result).toStrictEqual({ error: expect.any(String) });
+      const token2 = createUser('mubashir@gmail.com', '2password', 'Mubashir', 'Hussain').token;
+      const quizId2 = createQuiz(token2, 'quizName2', 'description').quizId;
+      const quizInfo = request('GET', SERVER_URL + `v1/admin/quiz/${quizId2}`, { qs: { token } });
+      expect(JSON.parse(quizInfo.body.toString())).toStrictEqual({ error: expect.any(String) });
     });
   });
 
   describe('Success Cases', () => {
-    let authUserId;
+    let token;
     let quizId;
 
     beforeEach(() => {
-      authUserId = createUser('hayden@gmail.com', '1password', 'Hayden', 'Smith');
-      quizId = createQuiz(createUser.authUserId, 'quizName', 'description');
+      token = createUser('hayden@gmail.com', '1password', 'Hayden', 'Smith');
+      quizId = createQuiz(token, 'quizName', 'description');
     });
 
     test('Successfully Returned quizInfo', () => {
-      const quizInfo = request('GET', SERVER_URL + 'v1/admin/quiz/:quizid', { qs: { authUserId } });
+      const quizInfo = request('GET', SERVER_URL + 'v1/admin/quiz/:quizid', { qs: { token } });
       expect(JSON.parse(quizInfo.body.toString())).toStrictEqual(
         {
           quizId: quizId,
@@ -84,28 +84,14 @@ describe('adminQuizInfo Tests', () => {
           numQuestions: expect.any(Number),
 
           questions: [
-            {
-              questionId: expect.any(Number),
-              question: expect.any(String),
-              duration: expect.any(Number),
-              points: expect.any(Number),
-              answers: [
-                {
-                  answerId: expect.any(Number),
-                  answer: expect.any(String),
-                  colour: expect.any(String),
-                  correct: expect.toBeTrue()
-                }
-              ]
-            },
           ]
         }
       );
     });
 
     test('Successfully Returned quizInfo after quizNameUpdate', () => {
-      quizNameUpdate(1243, quizId, 'newName');
-      const updatedQuizInfo = request('GET', SERVER_URL + 'v1/admin/quiz/:quizid', { qs: { authUserId } });
+      quizNameUpdate(token, quizId, 'newName');
+      const updatedQuizInfo = request('GET', SERVER_URL + 'v1/admin/quiz/:quizid', { qs: { token } });
       expect(updatedQuizInfo).toStrictEqual(
         {
           quizId: quizId,
@@ -116,20 +102,6 @@ describe('adminQuizInfo Tests', () => {
           numQuestions: expect.any(Number),
 
           questions: [
-            {
-              questionId: expect.any(Number),
-              question: expect.any(String),
-              duration: expect.any(Number),
-              points: expect.any(Number),
-              answers: [
-                {
-                  answerId: expect.any(Number),
-                  answer: expect.any(String),
-                  colour: expect.any(String),
-                  correct: expect.toBeTrue()
-                }
-              ]
-            },
           ]
         }
       );

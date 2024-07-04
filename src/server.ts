bf4975634ab409543026dd14616764e11ff98f82
Nafis Syed <z5362173@ad.unsw.edu.au>
2024-07-04 20:08:18 +1000

    Fixed code errors.

--- src/server.ts
+++ src/server.ts
@@ -43,7 +43,7 @@ app.get('/echo', (req: Request, res: Response) => {
 app.delete('/v1/clear', (req : Request, res: Response) => {
   const result = clear();
   if ('error' in result) {
-    return res.status(400).json(result)
+    return res.status(400).json(result);
   }
   res.json(result);
 });

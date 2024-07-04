c27540e8b00d58054e590ceafa8daf27a1d6e9a4
Parampreet Kaur <z5478214@ad.unsw.edu.au>
2024-07-04 19:09:28 +1000

    fixing auto linting errors

--- tests/adminQuizNameUpdate.test.js
+++ tests/adminQuizNameUpdate.test.js
@@ -1,122 +1,117 @@
-import { adminQuizInfo, adminQuizNameUpdate } from "../src/quiz.js";
-import { adminAuthRegister } from "../src/auth.js";
-import { adminQuizCreate } from "../src/quiz.js";
-import { clear } from "../src/other.js";
+import { adminQuizInfo, adminQuizNameUpdate } from '../src/quiz.js';
+import { adminAuthRegister } from '../src/auth.js';
+import { adminQuizCreate } from '../src/quiz.js';
+import { clear } from '../src/other.js';
 
 beforeEach(() => {
   clear();
 });
 
-describe ("adminQuizNameUpdate Tests", () => {
-  describe ("Error Cases", () => {
+describe('adminQuizNameUpdate Tests', () => {
+  describe('Error Cases', () => {
     let authUserId;
     let quizId;
-   
-    beforeEach( () => {
-      authUserId = adminAuthRegister("email@gmail.com", "1password" , "firstname", "lastname").authUserId;
-      quizId = adminQuizCreate(authUserId, "quizname", "description").quizId
-      
+
+    beforeEach(() => {
+      authUserId = adminAuthRegister('email@gmail.com', '1password', 'firstname', 'lastname').authUserId;
+      quizId = adminQuizCreate(authUserId, 'quizname', 'description').quizId;
     });
 
     test.each([
-      
+
       {
-        testName : "Check fail for empty input",
-        quizName : " ",
-        errorMessage : expect.any(String) ,
+        testName: 'Check fail for empty input',
+        quizName: ' ',
+        errorMessage: expect.any(String),
       },
       {
-        testName : "Check fail on short names" ,
-        quizName : "a",
-        errorMessage : expect.any(String),
+        testName: 'Check fail on short names',
+        quizName: 'a',
+        errorMessage: expect.any(String),
       },
       {
-        testName : "Check fail on short names" ,
-        quizName : "ab",
-        errorMessage : expect.any(String),
+        testName: 'Check fail on short names',
+        quizName: 'ab',
+        errorMessage: expect.any(String),
       },
       {
-        testName : "Check fail on short names" ,
-        quizName : "abc",
-        errorMessage : expect.any(String),
+        testName: 'Check fail on short names',
+        quizName: 'abc',
+        errorMessage: expect.any(String),
       },
       {
-        testName : "Check fail for names longer than 30 characters" ,
-        quizName : "abcdefghijklmnopqrstuvwxyzabcde",
-        errorMessage : expect.any(String) ,
+        testName: 'Check fail for names longer than 30 characters',
+        quizName: 'abcdefghijklmnopqrstuvwxyzabcde',
+        errorMessage: expect.any(String),
       },
       {
-        testName : "Check fail for quiz name with symbols" ,
-        quizName : "&",
-        errorMessage : expect.any(String) ,
+        testName: 'Check fail for quiz name with symbols',
+        quizName: '&',
+        errorMessage: expect.any(String),
       }
 
-    ])("Test $# => $testName", ({quizName, errorMessage}) => {
+    ])('Test $# => $testName', ({ quizName, errorMessage }) => {
       const name = adminQuizNameUpdate(authUserId, quizId, quizName);
-      expect(name).toStrictEqual( {error :  errorMessage});
-      
+      expect(name).toStrictEqual({ error: errorMessage });
     });
 
-    //Testing Invalid User id and Quiz id
-    test ("Invalid User id", () => {
-      const name = adminQuizNameUpdate(authUserId + 1, quizId, "Name");
-      expect(name).toStrictEqual( {error : expect.any(String)});
+    // Testing Invalid User id and Quiz id
+    test('Invalid User id', () => {
+      const name = adminQuizNameUpdate(authUserId + 1, quizId, 'Name');
+      expect(name).toStrictEqual({ error: expect.any(String) });
     });
-  
-    test ("Invalid Quiz id", () =>{
-      const name = adminQuizNameUpdate(authUserId, quizId + 1, "Name");
-      expect(name).toStrictEqual( {error : expect.any(String)});
+
+    test('Invalid Quiz id', () => {
+      const name = adminQuizNameUpdate(authUserId, quizId + 1, 'Name');
+      expect(name).toStrictEqual({ error: expect.any(String) });
     });
 
     test('Quiz Id does not refer to a quiz that this user owns', () => {
-      const name = adminQuizNameUpdate(authUserId + 1, quizId, "Name");
+      const name = adminQuizNameUpdate(authUserId + 1, quizId, 'Name');
       expect(name).toStrictEqual({ error: expect.any(String) });
     });
 
     test('Name is already used by the current logged in user for another quiz', () => {
-      adminQuizCreate(authUserId, "anotherQuizName", "description2").quizId;
-      const nameUpdate = adminQuizNameUpdate(authUserId, quizId, "anotherQuizName");
+      adminQuizCreate(authUserId, 'anotherQuizName', 'description2').quizId;
+      const nameUpdate = adminQuizNameUpdate(authUserId, quizId, 'anotherQuizName');
       expect(nameUpdate).toStrictEqual({ error: 'Name is already used' });
-
     });
-
   });
-  
 
-  describe ("Success Cases", () => {
+  describe('Success Cases', () => {
     let authUserId;
     let quizId;
-  
-    beforeEach( () => {
-      authUserId = adminAuthRegister("email@gmail.com", "1password", "firstname", "lastname").authUserId;
+
+    beforeEach(() => {
+      authUserId = adminAuthRegister('email@gmail.com', '1password', 'firstname', 'lastname').authUserId;
       quizId = adminQuizCreate(authUserId, 'name', 'description').quizId;
     });
 
-    test ("Check that function returns empty object", () => {
-      const name = adminQuizNameUpdate(authUserId, quizId, "Name");
+    test('Check that function returns empty object', () => {
+      const name = adminQuizNameUpdate(authUserId, quizId, 'Name');
       expect(name).toStrictEqual({});
     });
 
-    test ("Check name has been updated successfully", () => {
-      adminQuizNameUpdate(authUserId, quizId, "newName");
+    test('Check name has been updated successfully', () => {
+      adminQuizNameUpdate(authUserId, quizId, 'newName');
       const updatedQuizInfo = adminQuizInfo(authUserId, quizId);
       expect(updatedQuizInfo).toStrictEqual({
         quizId: quizId,
-        name: "newName",
+        name: 'newName',
         timeCreated: expect.any(Number),
         timeLastEdited: expect.any(Number),
         description: 'description'
       });
     });
 
-    test ("Successfully Returned quizInfo after quizNameUpdate", () =>{
-      adminQuizNameUpdate(authUserId, quizId, "newName");
+    test('Successfully Returned quizInfo after quizNameUpdate', () => {
+      adminQuizNameUpdate(authUserId, quizId, 'newName');
       const updatedQuizInfo = adminQuizInfo(authUserId, quizId);
-    
-      expect(updatedQuizInfo).toStrictEqual (
+
+      expect(updatedQuizInfo).toStrictEqual(
         {
           quizId: quizId,
-          name: "newName",
+          name: 'newName',
           timeCreated: expect.any(Number),
           timeLastEdited: expect.any(Number),
           description: 'description'
@@ -125,6 +120,3 @@ describe ("adminQuizNameUpdate Tests", () => {
     });
   });
 });
-
-
-
--- tests/adminUserDetailsUpdate.test.js
+++ tests/adminUserDetailsUpdate.test.js
@@ -1,5 +1,5 @@
-import { adminUserDetailsUpdate, adminAuthRegister } from '../src/auth.js'
-import { clear } from '../src/other.js'
+import { adminUserDetailsUpdate, adminAuthRegister } from '../src/auth.js';
+import { clear } from '../src/other.js';
 
 let id;
 let user;
@@ -19,20 +19,20 @@ describe('Testing adminUserDetailsUpdate for errors', () => {
   // Email is currently used by another user (excluding the current authorised user)
   test('Email is already used by another user', () => {
     const authUserId2 = adminAuthRegister('steph@unsw.edu.au', 'Farmingsimulator!1234', 'steph', 'liang').authUserId;
-    const result = adminUserDetailsUpdate (authUserId2, 'amelia@unsw.edu.au', 'steph', 'liang');
+    const result = adminUserDetailsUpdate(authUserId2, 'amelia@unsw.edu.au', 'steph', 'liang');
     expect(result).toStrictEqual({ error: 'email used by another user' });
   });
 
   // Email is not valid
   test('Email is not a valid email', () => {
     const result = adminUserDetailsUpdate(id, 'gurigiurabgiurag', 'amelia', 'su');
-    expect(result).toStrictEqual({ error: 'invalid email address' })
+    expect(result).toStrictEqual({ error: 'invalid email address' });
   });
 
   // First/last name contains invalid characters
   test('First name contains invalid characters', () => {
     const result = adminUserDetailsUpdate(id, 'amelia@unsw.ed.au', 'a!melia', 'su');
-    expect(result).toStrictEqual({ error: 'first name contains invalid characters' })
+    expect(result).toStrictEqual({ error: 'first name contains invalid characters' });
   });
 
   test('Last name contains invalid characters', () => {
@@ -48,7 +48,7 @@ describe('Testing adminUserDetailsUpdate for errors', () => {
 
   test('First name is too long', () => {
     const result = adminUserDetailsUpdate(id, 'amelia@unsw.ed.au', 'abcdefghijklmnopqrstuv', 'su');
-    expect(result).toStrictEqual({ error: 'first name is too long'});
+    expect(result).toStrictEqual({ error: 'first name is too long' });
   });
 
   test('Last name is too short', () => {

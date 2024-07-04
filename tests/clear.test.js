f511479a17f1332173ab63876e2039981537625f
Nafis Syed <z5362173@ad.unsw.edu.au>
2024-07-04 15:52:24 +1000

    Working around branches

---
+++ tests/clear.test.js
@@ -0,0 +1,39 @@
+import { clear } from '../src/other.js';
+
+beforeEach(() => {
+  clear();
+});
+
+describe("clear Function Tests", () => {
+  describe("Success Cases", () => {
+    test('Clear function should return an empty object', () => {
+      const result = clear();
+      expect(result).toEqual({});
+    });
+
+    test.each([
+      ['first call'],
+      ['second call'],
+      ['third call']
+    ])('Clear function should return an empty object on %s', (callEmpty) => {
+      const result = clear();
+      expect(result).toEqual({});
+    });
+
+    test('Clear function should not throw errors when called multiple times', () => {
+      expect(() => clear()).not.toThrow();
+      expect(() => clear()).not.toThrow();
+      expect(() => clear()).not.toThrow();
+    });
+  });
+});
+
+describe("Error Case", () => {
+  test('Clear function should handle undefined or null input', () => {
+    const result = clear(undefined);
+    expect(result).toEqual({});
+    
+    const result2 = clear(null);
+    expect(result2).toEqual({});
+  });
+});

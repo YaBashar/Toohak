92342ff0abc7d807837de94f322418abe21e24f0
Amelia Su <z5478980@ad.unsw.edu.au>
2024-07-05 11:51:01 +1000

    changed const to let

--- tests/adminUserPasswordUpdate.test.ts
+++ tests/adminUserPasswordUpdate.test.ts
@@ -4,8 +4,8 @@ import { port, url } from '../src/config.json';
 const SERVER_URL = `${url}:${port}`;
 const TIMEOUT_MS = 5 * 1000;
 
-const user = request('POST', SERVER_URL + '/v1/admin/auth/register', { json: {email: 'amelia@unsw.edu.au', password: 'abcd1234!@#$ABCD', nameFirst: 'amelia', nameLast: 'su'}});
-const id = JSON.parse(user.body.toString()).authUserId;
+let user = request('POST', SERVER_URL + '/v1/admin/auth/register', { json: {email: 'amelia@unsw.edu.au', password: 'abcd1234!@#$ABCD', nameFirst: 'amelia', nameLast: 'su'}});
+let id = JSON.parse(user.body.toString()).authUserId;
 
 beforeEach(() => {
   request('DELETE', SERVER_URL + '/v1/clear', { timeout: TIMEOUT_MS });

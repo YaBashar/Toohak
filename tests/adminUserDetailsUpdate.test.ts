bf127e82a14f8adc3be56cf73eca8ab3dda91cd0
Amelia Su <z5478980@ad.unsw.edu.au>
2024-07-08 11:03:00 +1000

    deleted extra authUserId mention

--- tests/adminUserDetailsUpdate.test.ts
+++ tests/adminUserDetailsUpdate.test.ts
@@ -18,51 +18,51 @@ describe('PUT /v1/admin/user/details', () => {
   test('Email is already used by another user', () => {
     const authUser2 = request('POST', SERVER_URL + '/v1/admin/auth/register', { json: {email: 'steph@unsw.edu.au', password: 'Farmingsimulator!1234', nameFirst: 'steph', nameLast: 'liang'}});
     const authUserId2 = JSON.parse(authUser2.body.toString()).authUserId;
-    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { authUserId: authUserId2, email: 'amelia@unsw.edu.au', nameFirst: 'amelia', nameLast: 'su' }});
+    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { authUserId2, email: 'amelia@unsw.edu.au', nameFirst: 'amelia', nameLast: 'su' }});
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: 'email used by another user' });
   });
 
   // Email is not valid
   test('Email is not a valid email', () => {
-    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { authUserId: token, email: 'gurigiurabgiurag', nameFirst: 'amelia', nameLast: 'su' }});
+    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { token, email: 'gurigiurabgiurag', nameFirst: 'amelia', nameLast: 'su' }});
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: 'invalid email address' });
   });
 
   // First/last name contains invalid characters
   test('First name contains invalid characters', () => {
-    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { authUserId: token, email: 'amelia@unsw.ed.au', nameFirst: 'a!melia', nameLast: 'su' }});
+    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { token, email: 'amelia@unsw.ed.au', nameFirst: 'a!melia', nameLast: 'su' }});
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: 'first name contains invalid characters' });
   });
 
   test('Last name contains invalid characters', () => {
-    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { authUserId: token, email: 'amelia@unsw.ed.au', nameFirst: 'amelia', nameLast: 'su+' }});
+    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { token, email: 'amelia@unsw.ed.au', nameFirst: 'amelia', nameLast: 'su+' }});
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: 'last name contains invalid characters' });
   });
 
   // First/last name is too short or too long
   test('First name is too short', () => {
-    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { authUserId: token, email: 'amelia@unsw.ed.au', nameFirst: 'a', nameLast: 'su' }});
+    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { token, email: 'amelia@unsw.ed.au', nameFirst: 'a', nameLast: 'su' }});
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: 'first name is too short' });
   });
 
   test('First name is too long', () => {
-    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { authUserId: token, email: 'amelia@unsw.ed.au', nameFirst: 'abcdefghijklmnopqrstuv', nameLast: 'su' }});
+    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { token, email: 'amelia@unsw.ed.au', nameFirst: 'abcdefghijklmnopqrstuv', nameLast: 'su' }});
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: 'first name is too long'});
   });
 
   test('Last name is too short', () => {
-    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { authUserId: token, email: 'amelia@unsw.ed.au', nameFirst: 'amelia', nameLast: 's' }});
+    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { token, email: 'amelia@unsw.ed.au', nameFirst: 'amelia', nameLast: 's' }});
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: 'last name is too short' });
   });
 
   test('Last name is too long', () => {
-    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { authUserId: token, email: 'amelia@unsw.ed.au', nameFirst: 'amelia', nameLast: 'abcdefghijklmnopqrstuv' }});
+    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { token, email: 'amelia@unsw.ed.au', nameFirst: 'amelia', nameLast: 'abcdefghijklmnopqrstuv' }});
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: 'last name is too long' });
   });
 
   // successful use of function
   test('function used correctly', () => {
-    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { authUserId: token, email: 'amelia1@unsw.ed.au', nameFirst: 'amelia', nameLast: 'su' }});
+    const res = request('PUT', SERVER_URL + '/v1/admin/user/details', { json: { token, email: 'amelia1@unsw.ed.au', nameFirst: 'amelia', nameLast: 'su' }});
     expect(JSON.parse(res.body.toString())).toStrictEqual({});
   });
 });

0d053f27a94b1640b4458a40a6bd1c4f46f626fd
Parampreet Kaur <z5478214@ad.unsw.edu.au>
2024-07-09 21:22:15 +1000

    fixing merge conflict

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ import { getUserIdFromToken } from './helper';
 import { adminQuizNameUpdate, adminQuizTransfer } from './quiz';
 import { clear } from '../src/other.js';
 import { adminAuthRegister, adminAuthLogin, adminUserDetails, adminUserDetailsUpdate, adminUserPasswordUpdate, adminAuthLogout } from './auth';
-import { adminQuizCreate, adminQuizRemove, adminQuizList, adminQuizDescriptionUpdate, adminQuizInfo } from './quiz';
+import { adminQuizCreate, adminQuizRemove, adminQuizList, adminQuizDescriptionUpdate, adminQuizInfo, adminQuizQuestionCreate } from './quiz';
 
 // Set up web app
 const app = express();
@@ -278,6 +278,28 @@ app.post('/v1/admin/quiz/:quizid/transfer', (req : Request, res: Response) => {
     }
   }
   return res.status(200).json(quizTransfer);
+});
+
+// adminQuizQuestionCreate
+app.post('/v1/admin/quiz/:quizid/question', (req: Request, res: Response) => {
+  const { token, questionBody } = req.body;
+  const quizid = parseInt(req.params.quizid as string);
+  const authUserId = getUserIdFromToken(token);
+  if (!authUserId) {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+
+  const result = adminQuizQuestionCreate(authUserId, quizid, questionBody);
+  if ('error' in result) {
+    if (result.error === 'Invalid Token') {
+      return res.status(401).json(result);
+    } else if (result.error === 'Quiz Id not owned by the user' || result.error === 'Quiz does not exist') {
+      return res.status(403).json(result);
+    } else {
+      return res.status(400).json(result);
+    }
+  }
+  return res.status(200).json(result);
 });
 
 // ====================================================================

cb29c02cfcaebdf404a38e91d780875a05b3824b
Sidak Harnoor Singh <z5525050@ad.unsw.edu.au>
2024-07-12 19:27:18 +1000

    added wrapper functions to test files

--- src/tests/adminQuizQuestionCreate.test.ts
+++ src/tests/adminQuizQuestionCreate.test.ts
@@ -4,7 +4,7 @@ import { port, url } from '../config.json';
 const SERVER_URL = `${url}:${port}`;
 const TIMEOUT_MS = 5 * 1000;
 
-// wrapper function
+// wrapper functions
 
 const createQuiz = (token : string, name : string, description : string) => {
   const res = request('POST', SERVER_URL + '/v1/admin/quiz', {
@@ -12,6 +12,27 @@ const createQuiz = (token : string, name : string, description : string) => {
   });
   return JSON.parse(res.body.toString());
 };
+
+const createUser = (email: string, password: string, firstName: string, lastName: string) => {
+  return request('POST', SERVER_URL + '/v1/admin/auth/register', {
+    json: { email, password, nameFirst: firstName, nameLast: lastName }
+  });
+};
+
+const questionCreate = (token: string, quizid: number, question: string, duration: number, points: number, answers: object) => {
+  return request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
+    json: {
+      token,
+      questionBody: {
+        question,
+        duration,
+        points,
+        answers
+      }
+    }
+  });
+};
+
 /// ////////////////////////////////////////////////////////////////////////////
 
 beforeEach(() => {
@@ -19,391 +40,249 @@ beforeEach(() => {
 });
 
 describe('POST /v1/admin/quiz/:quizid/question', () => {
-  // let token: string;
   let quizid: number;
   let quizid2: number;
   let token1: string;
   let token2: string;
 
   beforeEach(() => {
-    const uid1 = request('POST', SERVER_URL + '/v1/admin/auth/register', { json: { email: 'z5525050@unsw.edu.au', password: '123ABCabc@#$', nameFirst: 'sidak', nameLast: 'singh' } });
+    const uid1 = createUser('z5525050@unsw.edu.au', '123ABCabc@#$', 'sidak', 'singh');
     token1 = JSON.parse(uid1.body.toString()).token;
     quizid = createQuiz(token1, 'quizName', 'description').quizId;
 
-    const uid2 = request('POST', SERVER_URL + '/v1/admin/auth/register', { json: { email: 'z5555555@unsw.edu.au', password: 'abs@#$234', nameFirst: 'brim', nameLast: 'johnson' } });
+    const uid2 = createUser('z5555555@unsw.edu.au', 'abs@#$234', 'brim', 'johnson');
     token2 = JSON.parse(uid2.body.toString()).token;
-
-    quizid2 = createQuiz(token2, 'quizName2', 'description').quizid;
+    quizid2 = createQuiz(token2, 'quizName2', 'description').quizId;
   });
 
   // Token is empty or invalid (does not refer to valid logged in user session)
   test('Token is invalid (does not refer to valid logged in user session)', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
-      json: {
-        token: 'invalid token',
-        questionBody: {
-          question: 'Who is the Monarch of England?',
-          duration: 4,
-          points: 5,
-          answers: [
-            {
-              answer: 'Prince Charles',
-              correct: true,
-            },
-            {
-              answer: 'Queen Elizabeth',
-              correct: false,
-            }
-          ]
-        }
+    const res = questionCreate('invalidToken', quizid, 'Who is the Monarch of England?', 4, 5, [
+      {
+        answer: 'Prince Charles',
+        correct: true,
+      },
+      {
+        answer: 'Queen Elizabeth',
+        correct: false,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(401);
   });
 
   // token is empty
   test('Token is empty', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
-      json: {
-        token: '',
-        questionBody: {
-          question: 'Who is the Monarch of England?',
-          duration: 4,
-          points: 5,
-          answers: [
-            {
-              answer: 'Prince Charles',
-              correct: true,
-            },
-            {
-              answer: 'Queen Elizabeth',
-              correct: false,
-            }
-          ]
-        }
+    const res = questionCreate('', quizid, 'Who is the Monarch of England?', 4, 5, [
+      {
+        answer: 'Prince Charles',
+        correct: true,
+      },
+      {
+        answer: 'Queen Elizabeth',
+        correct: false,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(401);
   });
 
   // Question string is less than 5 characters
   test('Question string is less than 5 characters', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
-      json: {
-        token: token1,
-        questionBody: {
-          question: 'Who',
-          duration: 4,
-          points: 5,
-          answers: [
-            {
-              answer: 'Prince Charles',
-              correct: true,
-            }
-          ]
-        }
+    const res = questionCreate(token1, quizid, 'Who', 4, 5, [
+      {
+        answer: 'Prince Charles',
+        correct: true,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   // Question string is greater than 50 characters in length
   test('Question string is greater than 50 characters in length', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
-      json: {
-        token: token1,
-        questionBody: {
-          question: 'Who is the Monarch of England? Who is the Monarch of England? Who is the Monarch of England?',
-          duration: 4,
-          points: 5,
-          answers: [
-            {
-              answer: 'Prince Charles',
-              correct: true,
-            }
-          ]
-        }
+    const res = questionCreate(token1, quizid, 'Who is the Monarch of England? Who is the Monarch of England? Who is the Monarch of England?', 4, 5, [
+      {
+        answer: 'Prince Charles',
+        correct: true,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   // The question has more than 6 answers
   test('The question has more than 6 answers', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
-      json: {
-        token: token1,
-        questionBody: {
-          question: 'Who is the Monarch of England?',
-          duration: 4,
-          points: 5,
-          answers: [
-            {
-              answer: 'Prince Charles',
-              correct: true,
-            },
-            {
-              answer: 'Queen Elizabeth',
-              correct: false,
-            },
-            {
-              answer: 'Prince William',
-              correct: false,
-            },
-            {
-              answer: 'Prince Harry',
-              correct: false,
-            },
-            {
-              answer: 'Prince Philip',
-              correct: false,
-            },
-            {
-              answer: 'Prince Andrew',
-              correct: false,
-            },
-            {
-              answer: 'Prince Edward',
-              correct: false,
-            }
-          ]
-        }
+    const res = questionCreate(token1, quizid, 'Who is the Monarch of England?', 4, 5, [
+      {
+        answer: 'Prince Charles',
+        correct: true,
+      },
+      {
+        answer: 'Queen Elizabeth',
+        correct: false,
+      },
+      {
+        answer: 'Prince William',
+        correct: false,
+      },
+      {
+        answer: 'Prince Harry',
+        correct: false,
+      },
+      {
+        answer: 'Prince Philip',
+        correct: false,
+      },
+      {
+        answer: 'Prince Andrew',
+        correct: false,
+      },
+      {
+        answer: 'Prince Edward',
+        correct: false,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   // The question has less than 2 answers
   test('The question has less than 2 answers', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
-      json: {
-        token: token1,
-        questionBody: {
-          question: 'Who is the Monarch of England?',
-          duration: 4,
-          points: 5,
-          answers: [
-            {
-              answer: 'Prince Charles',
-              correct: true,
-            }
-          ]
-        }
+    const res = questionCreate(token1, quizid, 'Who is the Monarch of England?', 4, 5, [
+      {
+        answer: 'Prince Charles',
+        correct: true,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   // The question duration is not a positive number
   test('The question duration is not a positive number', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
-      json: {
-        token: token1,
-        questionBody: {
-          question: 'Who is the Monarch of England?',
-          duration: -4,
-          points: 5,
-          answers: [
-            {
-              answer: 'Prince Charles',
-              correct: true,
-            },
-            {
-              answer: 'Queen Elizabeth',
-              correct: false,
-            }
-          ]
-        }
+    const res = questionCreate(token1, quizid, 'Who is the Monarch of England?', -4, 5, [
+      {
+        answer: 'Prince Charles',
+        correct: true,
+      },
+      {
+        answer: 'Queen Elizabeth',
+        correct: false,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   // The sum of the question durations in the quiz exceeds 3 minutes
   test('The sum of the question durations in the quiz exceeds 3 minutes', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
-      json: {
-        token: token1,
-        questionBody: {
-          question: 'Who is the Monarch of England?',
-          duration: 190,
-          points: 5,
-          answers: [
-            {
-              answer: 'Prince Charles',
-              correct: true,
-            },
-            {
-              answer: 'Queen Elizabeth',
-              correct: false,
-            }
-          ]
-        }
+    const res = questionCreate(token1, quizid, 'Who is the Monarch of England?', 190, 5, [
+      {
+        answer: 'Prince Charles',
+        correct: true,
+      },
+      {
+        answer: 'Queen Elizabeth',
+        correct: false,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   // The points awarded for the question are less than 1
   test('The points awarded for the question are less than 1', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
-      json: {
-        token: token1,
-        questionBody: {
-          question: 'Who is the Monarch of England?',
-          duration: 4,
-          points: 0,
-          answers: [
-            {
-              answer: 'Prince Charles',
-              correct: true,
-            },
-            {
-              answer: 'Queen Elizabeth',
-              correct: false,
-            }
-          ]
-        }
+    const res = questionCreate(token1, quizid, 'Who is the Monarch of England?', 4, 0, [
+      {
+        answer: 'Prince Charles',
+        correct: true,
+      },
+      {
+        answer: 'Queen Elizabeth',
+        correct: false,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   // The points awarded for the question are greater than 10
   test('The points awarded for the question are greater than 10', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
-      json: {
-        token: token1,
-        questionBody: {
-          question: 'Who is the Monarch of England?',
-          duration: 4,
-          points: 11,
-          answers: [
-            {
-              answer: 'Prince Charles',
-              correct: true,
-            },
-            {
-              answer: 'Queen Elizabeth',
-              correct: false,
-            }
-          ]
-        }
+    const res = questionCreate(token1, quizid, 'Who is the Monarch of England?', 4, 11, [
+      {
+        answer: 'Prince Charles',
+        correct: true,
+      },
+      {
+        answer: 'Queen Elizabeth',
+        correct: false,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   // The length of any answer is shorter than 1 character long
   test('The length of any answer is shorter than 1 character long', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
-      json: {
-        token: token1,
-        questionBody: {
-          question: 'Who is the Monarch of England?',
-          duration: 4,
-          points: 5,
-          answers: [
-            {
-              answer: 'hello',
-              correct: false,
-            },
-            {
-              answer: '',
-              correct: true,
-            }
-          ]
-        }
+    const res = questionCreate(token1, quizid, 'Who is the Monarch of England?', 4, 5, [
+      {
+        answer: 'hello',
+        correct: false,
+      },
+      {
+        answer: '',
+        correct: true,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   // The length of any answer is longer than 30 characters long
   test('The length of any answer is longer than 30 characters long', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
-      json: {
-        token: token1,
-        questionBody: {
-          question: 'Who is the Monarch of England?',
-          duration: 4,
-          points: 5,
-          answers: [
-            {
-              answer: 'Prince Charles Prince Charles Prince Charles',
-              correct: true,
-            },
-            {
-              answer: 'Queen Elizabeth Queen Elizabeth Queen Elizabeth',
-              correct: false,
-            }
-          ]
-        }
+    const res = questionCreate(token1, quizid, 'Who is the Monarch of England?', 4, 5, [
+      {
+        answer: 'Prince Charles Prince Charles Prince Charles',
+        correct: true,
+      },
+      {
+        answer: 'Queen Elizabeth Queen Elizabeth Queen Elizabeth',
+        correct: false,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   // Any answer strings are duplicates of one another (within the same question)
   test('Any answer strings are duplicates of one another (within the same question)', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
-      json: {
-        token: token1,
-        questionBody: {
-          question: 'Who is the Monarch of England?',
-          duration: 4,
-          points: 5,
-          answers: [
-            {
-              answer: 'Prince Charles',
-              correct: true,
-            },
-            {
-              answer: 'Prince Charles',
-              correct: false,
-            }
-          ]
-        }
+    const res = questionCreate(token1, quizid, 'Who is the Monarch of England?', 4, 5, [
+      {
+        answer: 'Prince Charles',
+        correct: true,
+      },
+      {
+        answer: 'Prince Charles',
+        correct: false,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
-  // There are no correct answers
-  test('There are no correct answers', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
-      json: {
-        token: token1,
-        questionBody: {
-          question: 'Who is the Monarch of England?',
-          duration: 4,
-          points: 5,
-          answers: [
-            {
-              answer: 'Prince Charles',
-              correct: false,
-            },
-            {
-              answer: 'Prince',
-              correct: false,
-            }
-          ]
-        }
+  // No answer is marked correct
+  test('No answer is marked correct', () => {
+    const res = questionCreate(token1, quizid, 'Who is the Monarch of England?', 4, 5, [
+      {
+        answer: 'Prince Charles',
+        correct: false,
+      },
+      {
+        answer: 'Queen Elizabeth',
+        correct: false,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
@@ -434,58 +313,34 @@ describe('POST /v1/admin/quiz/:quizid/question', () => {
     expect(res.statusCode).toBe(403);
   });
 
-  // test to check if quiz ID does not refer to a quiz that this user owns
-  test('Quiz ID does not refer to a quiz that this user owns', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid2}/question`, {
-      json: {
-        token: token1,
-        questionBody: {
-          question: 'Who is the Monarch of England?',
-          duration: 4,
-          points: 5,
-          answers: [
-            {
-              answer: 'Prince Charles',
-              correct: true,
-            },
-            {
-              answer: 'Queen Elizabeth',
-              correct: false,
-            }
-          ]
-        }
+  // The provided quizid does not belong to the user
+  test('The provided quizid does not belong to the user', () => {
+    const res = questionCreate(token1, quizid2, 'Who is the Monarch of England?', 4, 5, [
+      {
+        answer: 'Prince Charles',
+        correct: true,
+      },
+      {
+        answer: 'Queen Elizabeth',
+        correct: false,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(403);
   });
 
-  // quiz question created successfully
-  test('Quiz question created successfully', () => {
-    const res = request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/question`, {
-      json: {
-        token: token1,
-        questionBody: {
-          question: 'Who is the Monarch of England?',
-          duration: 4,
-          points: 5,
-          answers: [
-            {
-              answer: 'Prince Charles',
-              correct: false,
-            },
-            {
-              answer: 'Prince is not Charles',
-              correct: true,
-            },
-            {
-              answer: 'Prince is Beckham',
-              correct: false,
-            }
-          ]
-        }
+  // Valid input
+  test('Valid input', () => {
+    const res = questionCreate(token1, quizid, 'Who is the Monarch of England?', 4, 5, [
+      {
+        answer: 'Prince Charles',
+        correct: true,
+      },
+      {
+        answer: 'Queen Elizabeth',
+        correct: false,
       }
-    });
+    ]);
     expect(JSON.parse(res.body.toString())).toStrictEqual({ questionId: expect.any(Number) });
     expect(res.statusCode).toBe(200);
   });

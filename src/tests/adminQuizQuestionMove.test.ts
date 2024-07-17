43475845f61bf2f92493d992eb50b2076e6ac030
Amelia Su <z5478980@ad.unsw.edu.au>
2024-07-17 21:46:12 +1000

    added wrapper functions for test file

--- src/tests/adminQuizQuestionMove.test.ts
+++ src/tests/adminQuizQuestionMove.test.ts
@@ -11,179 +11,145 @@ let randomToken: string;
 let randomQuizId: string;
 let quiz1;
 
+// wrapper functions
+const createUser = (email: string, password: string, nameFirst: string, nameLast: string) => {
+  const res = request('POST', SERVER_URL + '/v1/admin/auth/register', {
+    json: { email, password, nameFirst, nameLast }
+  });
+  return JSON.parse(res.body.toString());
+}
+
+const userLogin = (email: string, password: string) => {
+  const res = request('POST', SERVER_URL + '/v1/admin/auth/login', {
+    json: { email, password }
+  });
+  return JSON.parse(res.body.toString());
+}
+
+const createQuiz = (token: string, name: string, description: string) => {
+  const res = request('POST', SERVER_URL + '/v1/admin/quiz', {
+    json: { token, name, description }
+  });
+  return JSON.parse(res.body.toString());
+}
+
+const addQuestion = (token: string, quizid: string, question: string, duration: number, points: number, answers: object) => {
+  const res = request('POST', SERVER_URL + '/v1/admin/quiz/${quizid}/question', {
+    json: {
+      token,
+      questionBody: {
+        question,
+        duration,
+        points,
+        answers
+      }
+    }
+  });
+  return JSON.parse(res.body.toString());
+}
+
+const moveQuestion = (token: string, quizid: string, questionid: string, newPosition: number) => {
+  const res = request('PUT', SERVER_URL + '/v1/admin/quiz/${quizid}/question/${questionid}/move', {
+    json: {
+      token,
+      newPosition
+    }
+  });
+  return JSON.parse(res.body.toString());
+}
+
+
 beforeEach(() => {
   request('DELETE', SERVER_URL + '/v1/clear', { timeout: TIMEOUT_MS });
 
   // create account and log in
-  const user = request('POST', SERVER_URL + '/v1/admin/auth/register', {
-    json: { email: 'amelia@unsw.edu.au', password: 'abcd1234!@#$ABCD', nameFirst: 'amelia', nameLast: 'su' }
-  });
-  token = JSON.parse(user.body.toString()).token;
-  request('POST', SERVER_URL + '/v1/admin/auth/login', {
-    json: { email: 'amelia@unsw.edu.au', password: 'abcd1234!@#$ABCD' }
-  });
+  const user = createUser('amelia@unsw.edu.au', 'abcd1234!@#$ABCD', 'amelia', 'su')
+  token = user.token;
+  userLogin('amelia@unsw.edu.au', 'abcd1234!@#$ABCD')
 
   // create a quiz
-  quiz1 = request('POST', SERVER_URL + '/v1/admin/quiz', {
-    json: { token, name: 'quiz 1', description: 'the first quiz' }
-  });
-  quiz1Id = JSON.parse(quiz1.body.toString()).quizId;
+  quiz1 = createQuiz(token, 'quiz 1', 'the first quiz')
+  quiz1Id = quiz1.quizId;
 
   // add a question to the quiz1
-  const question1Quiz1 = request('POST', SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question`, {
-    json: {
-      token: token,
-      questionBody: {
-        question: 'Who is the Monarch of England?',
-        duration: 4,
-        points: 5,
-        answers: [
-          { answer: 'Prince William', correct: false },
-          { answer: 'Prince Charles', correct: true },
-          { answer: 'Prince Beckham', correct: false }
-        ]
-      }
-    }
-  });
-  question1Quiz1Id = JSON.parse(question1Quiz1.body.toString()).questionId;
+  const question1Quiz1 = addQuestion(token, quiz1Id, 'Who is the Monarch of England?', 4, 5,
+  [
+    { answer: 'Prince William', correct: false },
+    { answer: 'Prince Charles', correct: true },
+    { answer: 'Prince Beckham', correct: false }
+  ])
+  question1Quiz1Id = question1Quiz1.questionId;
 
   // add another question to quiz1
-  request('POST', SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question`, {
-    json: {
-      token: token,
-      questionBody: {
-        question: 'What is 10 - 7?',
-        duration: 4,
-        points: 5,
-        answers: [
-          { answer: '2', correct: false },
-          { answer: '3', correct: true },
-          { answer: '17', correct: false }
-        ]
-      }
-    }
-  });
+  addQuestion(token, quiz1Id, 'What is 10 - 7?', 4, 5,
+  [
+    { answer: '2', correct: false },
+    { answer: '3', correct: true },
+    { answer: '17', correct: false }
+  ])
 });
 
 describe('PUT /v1/admin/quiz/:quizid/question/:questionid/move', () => {
   test('Question id does not exist', () => {
-    const res = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question/${55}/move`, {
-      json: {
-        token,
-        newPosition: 2
-      }
-    });
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
-    expect(res.statusCode).toBe(400);
-  });
-
-  test('Question id does not exist', () => {
-    const res = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question/${55}/move`, {
-      json: {
-        token,
-        newPosition: 2
-      }
-    });
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = moveQuestion(token, quiz1Id, '55', 2)
+    expect(res).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   test('New position is less than 0', () => {
-    const res = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question/${question1Quiz1Id}/move`, {
-      json: {
-        token,
-        newPosition: -2
-      }
-    });
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = moveQuestion(token, quiz1Id, question1Quiz1Id, -2)
+    expect(res).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   test('New position is too big', () => {
-    const res = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question/${question1Quiz1Id}/move`, {
-      json: {
-        token,
-        newPosition: 5
-      }
-    });
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = moveQuestion(token, quiz1Id, question1Quiz1Id, 5)
+    expect(res).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   test('New position is current position', () => {
-    const res = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question/${question1Quiz1Id}/move`, {
-      json: {
-        token,
-        newPosition: 0
-      }
-    });
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = moveQuestion(token, quiz1Id, question1Quiz1Id, 0)
+    expect(res).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   test('empty token', () => {
-    const res = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question/${question1Quiz1Id}/move`, {
-      json: {
-        token: '',
-        newPosition: 2
-      }
-    });
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = moveQuestion('', quiz1Id, question1Quiz1Id, 2)
+    expect(res).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(401);
   });
 
   test('invalid token', () => {
     request('POST', SERVER_URL + '/v1/admin/auth/logout', { json: { token } });
-    const res = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question/${question1Quiz1Id}/move`, {
-      json: {
-        token: token,
-        newPosition: 2
-      }
-    });
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = moveQuestion(token, quiz1Id, question1Quiz1Id, 2)
+    expect(res).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(401);
   });
 
   // Valid token is provided, but user is not an owner of this quiz or quiz doesn't exist
   test('user is not an owner of this quiz', () => {
-    const user = request('POST', SERVER_URL + '/v1/admin/auth/register', { json: { email: 'random@unsw.edu.au', password: 'abcd1234!@#$ABC', nameFirst: 'ran', nameLast: 'dom' } });
-    randomToken = JSON.parse(user.body.toString()).token;
-    request('POST', SERVER_URL + '/v1/admin/auth/login', { json: { email: 'random@unsw.edu.au', password: 'abcd1234!@#$ABC' } });
-
-    const randomQuiz = request('POST', SERVER_URL + '/v1/admin/quiz', { json: { randomToken, name: 'random quiz', description: 'a random quiz' } });
-    randomQuizId = JSON.parse(randomQuiz.body.toString()).quizid;
+    const user = createUser('random@unsw.edu.au', 'abcd1234!@#$ABC', 'ran', 'dom')
+    randomToken = user.token;
+    userLogin('random@unsw.edu.au', 'abcd1234!@#$ABC')
+    const randomQuiz = createQuiz(randomToken, 'random quiz', 'a random quiz')
+    randomQuizId = randomQuiz.quizid;
     request('POST', SERVER_URL + '/v1/admin/auth/logout', { json: { email: 'random@unsw.edu.au', password: 'abcd1234!@#$ABC' } });
-
-    request('POST', SERVER_URL + '/v1/admin/auth/login', { json: { email: 'amelia@unsw.edu.au', password: 'abcd1234!@#$ABCD' } });
-    const res = request('PUT', SERVER_URL + `/v1/admin/quiz/${randomQuizId}/question/${question1Quiz1Id}/move`, {
-      json: {
-        token: token,
-        newPosition: 2
-      }
-    });
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    userLogin('amelia@unsw.edu.au', 'abcd1234!@#$ABCD')
+    const res = moveQuestion(token, randomQuizId, question1Quiz1Id, 2)
+    expect(res).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(403);
   });
 
   test('this quiz does not exist', () => {
-    const res = request('PUT', SERVER_URL + `/v1/admin/quiz/${999}/question/${question1Quiz1Id}/move`, {
-      json: {
-        token: token,
-        newPosition: 2
-      }
-    });
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = moveQuestion(token, '999', question1Quiz1Id, 2)
+    expect(res).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(403);
   });
 
   test('All fields are valid', () => {
-    const res = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1Id}/question/${question1Quiz1Id}/move`, {
-      json: {
-        token,
-        newPosition: 1
-      }
-    });
-    expect(JSON.parse(res.body.toString())).toStrictEqual({});
+    const res = moveQuestion(token, quiz1Id, question1Quiz1Id, 1)
+    expect(res).toStrictEqual({});
     expect(res.statusCode).toBe(200);
   });
 });

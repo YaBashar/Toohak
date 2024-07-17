da3d7f0ba1472e68fbb0bb5beb85d075034c49b8
Amelia Su <z5478980@ad.unsw.edu.au>
2024-07-17 20:42:19 +1000

    added wrapper functions to test files

--- src/tests/adminUserPasswordUpdate.test.ts
+++ src/tests/adminUserPasswordUpdate.test.ts
@@ -6,57 +6,73 @@ const TIMEOUT_MS = 5 * 1000;
 
 let token: string;
 
+// wrapper functions
+const createUser = (email: string, password: string, nameFirst: string, nameLast: string) => {
+  const res = request('POST', SERVER_URL + '/v1/admin/auth/register', {
+    json: { email, password, nameFirst, nameLast }
+  });
+  return JSON.parse(res.body.toString());
+}
+
+const updatePassword = (token: string, oldPassword: string, newPassword: string) => {
+  const res = request('PUT', SERVER_URL + '/v1/admin/user/password', {
+    json: { token, oldPassword, newPassword }
+  });
+  return JSON.parse(res.body.toString());
+}
+
+
 beforeEach(() => {
   request('DELETE', SERVER_URL + '/v1/clear', { timeout: TIMEOUT_MS });
-  const user = request('POST', SERVER_URL + '/v1/admin/auth/register', { json: { email: 'amelia@unsw.edu.au', password: 'abcd1234!@#$ABCD', nameFirst: 'amelia', nameLast: 'su' } });
-  token = JSON.parse(user.body.toString()).token;
+  const user = createUser('amelia@unsw.edu.au', 'abcd1234!@#$ABCD', 'amelia', 'su')
+  token = user.token;
 });
 
 describe('PUT /v1/admin/user/password', () => {
   // Old password is not correct
   test('Incorrect password', () => {
-    const res = request('PUT', SERVER_URL + '/v1/admin/user/password', { json: { token, oldPassword: 'abcd1234!@#$ABC', newPassword: 'newabcd1234!@#$ABCD' } });
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = updatePassword(token, 'abcd1234!@#$ABC', 'newabcd1234!@#$ABCD')
+    expect(res).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   // Old password and new password are the same
   test('Old password is the same as the new password', () => {
-    const res = request('PUT', SERVER_URL + '/v1/admin/user/password', { json: { token, oldPassword: 'abcd1234!@#$ABCD', newPassword: 'abcd1234!@#$ABCD' } });
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = updatePassword(token, 'abcd1234!@#$ABCD', 'abcd1234!@#$ABCD')
+    expect(res).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   // New password has been used before
   test('New password has been used before', () => {
-    const res = request('PUT', SERVER_URL + '/v1/admin/user/password', { json: { token, oldPassword: 'abcd1234!@#$ABCD', newPassword: 'abcd1234!@#$ABC' } });
+    const res = updatePassword(token, 'abcd1234!@#$ABCD', 'abcd1234!@#$ABC')
     expect(res.statusCode).toBe(200);
-    const res2 = request('PUT', SERVER_URL + '/v1/admin/user/password', { json: { token, oldPassword: 'abcd1234!@#$ABC', newPassword: 'abcd1234!@#$ABCD' } });
-    expect(JSON.parse(res2.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res2 = updatePassword(token, 'abcd1234!@#$ABC', 'abcd1234!@#$ABCD')
+    expect(res2).toStrictEqual({ error: expect.any(String) });
     expect(res2.statusCode).toBe(400);
   });
 
   // New password is too short
   test('Invalid password length', () => {
-    const res = request('PUT', SERVER_URL + '/v1/admin/user/password', { json: { token, oldPassword: 'abcd1234!@#$ABCD', newPassword: 'abcd123' } });
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = updatePassword(token, 'abcd1234!@#$ABCD', 'abcd123')
+    expect(res).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
   });
 
   // New password doesn't contain at least on number and one letter
   test('Password does not contain at least one number and one letter', () => {
-    const res = request('PUT', SERVER_URL + '/v1/admin/user/password', { json: { token, oldPassword: 'abcd1234!@#$ABCD', newPassword: 'abcdefgh' } });
-    expect(JSON.parse(res.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res = updatePassword(token, 'abcd1234!@#$ABCD', 'abcdefgh')
+    expect(res).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(400);
-    const res2 = request('PUT', SERVER_URL + '/v1/admin/user/password', { json: { token, oldPassword: 'abcd1234!@#$ABCD', newPassword: '12345678' } });
-    expect(JSON.parse(res2.body.toString())).toStrictEqual({ error: expect.any(String) });
+    const res2 = updatePassword(token, 'abcd1234!@#$ABCD', '12345678')
+    expect(res2).toStrictEqual({ error: expect.any(String) });
     expect(res2.statusCode).toBe(400);
   });
 
   // Success case
   test('Success case', () => {
-    const res = request('PUT', SERVER_URL + '/v1/admin/user/password', { json: { token, oldPassword: 'abcd1234!@#$ABCD', newPassword: 'abcd1234!@#$ABC' } });
-    expect(JSON.parse(res.body.toString())).toStrictEqual({});
+    const res = updatePassword(token, 'abcd1234!@#$ABCD', 'abcd1234!@#$ABC');
+    expect(res).toStrictEqual({});
     expect(res.statusCode).toBe(200);
   });
 });

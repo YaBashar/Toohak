b5f43525a0ddab1bd3772fa97914c9cf8a4b2b3a
Parampreet Kaur <z5478214@ad.unsw.edu.au>
2024-07-18 13:19:22 +1000

    created helper fuctions for finding the index of commonly searched values in helper.ts

--- src/helper.ts
+++ src/helper.ts
@@ -14,3 +14,40 @@ export function getUserIdFromToken(sessionId: string): number {
 
   return session.userId;
 }
+
+// returns the userId associated with a session 
+export function findUserIdFromSessionId(target: number): number {
+  const sessArr = getData().sessions;
+  return (sessArr.find(session => (session.sessionId === target))).userId;
+}
+
+// returns the index of user with userId in users array
+export function findUserIndexfromUserId(target: number): number {
+  const userArr = getData().users;
+  return (userArr.findIndex(user => (user.userId === target)));
+}
+
+// returns the index of user with email in users array
+export function findUserIndexfromEmail(target: string): number {
+  const userArr = getData().users;
+  return (userArr.findIndex(user => (user.email === target)));
+}
+
+// returns the index of session with sessionId in sessions array
+export function findSessionIndexfromSessionId(target: number): number {
+  const sessArr = getData().sessions;
+  return (sessArr.findIndex(session => (session.sessionId == target)));
+}
+
+// returns the index of quiz with quizId in quizzes array
+export function findQuizIndexfromQuizId(target: number): number {
+  const quizArr = getData().quizzes;
+  return (quizArr.findIndex(quiz => (quiz.quizId == target)));
+}
+
+// returns the index of quiz with quizId in trash array
+export function findDelQuizIndexfromQuizId(target: number): number {
+  const trashArr = getData().trash;
+  return (trashArr.findIndex(quiz => (quiz.quizId == target)));
+}
+

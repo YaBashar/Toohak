99fe807d4465080467ed3d0d70af31bbd9774758
Amelia Su <z5478980@ad.unsw.edu.au>
2024-07-18 16:09:43 +1000

    added wrapper functions

--- src/tests/adminQuizList.test.ts
+++ src/tests/adminQuizList.test.ts
@@ -11,44 +11,58 @@ const quizList = (token: string) => {
   const res = request('GET', SERVER_URL + '/v1/admin/quiz/list', {
     qs: { token }
   });
-  return JSON.parse(res.body.toString());
+  return { body: JSON.parse(res.body.toString()), statusCode: res.statusCode };
 }
 
 const createUser = (email: string, password: string, nameFirst: string, nameLast: string) => {
   const res = request('POST', SERVER_URL + '/v1/admin/auth/register', {
     json: { email, password, nameFirst, nameLast }
   });
-  return JSON.parse(res.body.toString());
+  return { body: JSON.parse(res.body.toString()), statusCode: res.statusCode };
 }
 
 const createQuiz = (token: string, name: string, description: string) => {
   const res = request('POST', SERVER_URL + '/v1/admin/quiz', {
     json: { token, name, description }
   });
-  return JSON.parse(res.body.toString());
+  return { body: JSON.parse(res.body.toString()), statusCode: res.statusCode };
+}
+
+const removeQuiz = (token: string, quizid: string) => {
+  const res = request('DELETE', SERVER_URL + '/v1/admin/quiz/${quizid}', {
+    json: { token, quizid }
+  });
+  return { body: JSON.parse(res.body.toString()), statusCode: res.statusCode };
+}
+
+const updateQuizName = (token: string, quizid: string, name: string) => {
+  const res = request('PUT', SERVER_URL + '/v1/admin/quiz/${quizid}/name', {
+    json: { token, quizid, name }
+  });
+  return { body: JSON.parse(res.body.toString()), statusCode: res.statusCode };
 }
 
 
 beforeEach(() => {
   request('DELETE', SERVER_URL + '/v1/clear', { timeout: TIMEOUT_MS });
   const user = createUser('amelia@unsw.edu.au', 'abcd1234!@#$ABCD', 'amelia', 'su')
-  token = user.token;
+  token = user.body.token;
 });
 
 describe('GET /v1/admin/quiz/list', () => {
   // AuthUserId isn't valid
   test('Invalid AuthUserId', () => {
     const res = quizList('randomstring');
-    expect(res).toStrictEqual({ error: expect.any(String) });
+    expect(res.body).toStrictEqual({ error: expect.any(String) });
     expect(res.statusCode).toBe(401);
     const res2 = quizList('1');
-    expect(res2).toStrictEqual({ error: expect.any(String) });
+    expect(res2.body).toStrictEqual({ error: expect.any(String) });
     expect(res2.statusCode).toBe(401);
   });
 
   test('Logged in user has no quizzes', () => {
     const res = quizList(token);
-    expect(res).toStrictEqual(
+    expect(res.body).toStrictEqual(
       {
         quizzes:
         [
@@ -61,20 +75,45 @@ describe('GET /v1/admin/quiz/list', () => {
     const quizId = createQuiz(token, 'quiz 1', 'the first quiz')
     const quiz2Id = createQuiz(token, 'quiz 2', 'the second quiz')
     const res = quizList(token);
-    expect(res).toStrictEqual(
+    expect(res.body).toStrictEqual(
       {
         quizzes:
         [
           {
-            quizId: quizId.quizId,
+            quizId: quizId.body.quizId,
             name: 'quiz 1'
           },
           {
-            quizId: quiz2Id.quizId,
+            quizId: quiz2Id.body.quizId,
             name: 'quiz 2'
           }
         ]
       });
     expect(res.statusCode).toBe(200);
+    // removeQuiz(token, quizId.body.quizId);
+    // const res2 = quizList(token);
+    // expect(res2.body).toStrictEqual(
+    //   {
+    //     quizzes:
+    //     [
+    //       {
+    //         quizId: quiz2Id.body.quizId,
+    //         name: 'quiz 2'
+    //       }
+    //     ]
+    //   });
+    // expect(res2.statusCode).toBe(200);
+    updateQuizName(token, quiz2Id.body.quizid, 'new name');
+    const res3 = quizList(token);
+    expect(res3.body).toStrictEqual(
+      {
+        quizzes:
+        [
+          {
+            quizId: quiz2Id.body.quizId,
+            name: 'new name'
+          }
+        ]
+      });
   });
 });

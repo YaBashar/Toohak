c2ae049f4bb7065cca4a833f5437c9607f4f331a
Parampreet Kaur <z5478214@ad.unsw.edu.au>
2024-07-28 13:09:47 +1000

    debugging and linting for adminGameSessionStart

--- src/v3tests/adminGameSessionStart.test.ts
+++ src/v3tests/adminGameSessionStart.test.ts
@@ -11,6 +11,7 @@ beforeEach(() => {
   request('DELETE', SERVER_URL + '/v1/clear', { timeout: TIMEOUT_MS });
   token = requestAuthRegister('zid@ad.unsw.edu.au', 'abcd1234', 'first', 'last');
   quizId = requestCreateQuiz(token, 'valid_name', 'valid_description');
+
   createQuizQuestion(token, quizId, 'Who is the Monarch of England?', 4, 5, [
     { answer: 'Prince Charles', correct: true }, { answer: 'Queen Elizabeth', correct: false }
   ]);
@@ -18,40 +19,40 @@ beforeEach(() => {
 
 afterEach(() => {
   request('DELETE', SERVER_URL + '/v1/clear', { timeout: TIMEOUT_MS });
-})
+});
 
 test('Token is empty or invalid', () => {
-  const res = requestCreateSession('invalid token', quizId, 3)
+  const res = requestCreateSession('invalid token', quizId, 3);
   const data = JSON.parse(res.body.toString());
 
   expect(data).toStrictEqual({ error: expect.any(String) });
   expect(res.statusCode).toStrictEqual(401);
-})
+});
 
 test('Valid token but user is not owner of the quiz', () => {
   const token2 = requestAuthRegister('zid2@ad.unsw.edu.au', 'abcd1234', 'first', 'last');
-  const res = requestCreateSession(token2, quizId, 3)
+  const res = requestCreateSession(token2, quizId, 3);
   const data = JSON.parse(res.body.toString());
 
   expect(data).toStrictEqual({ error: expect.any(String) });
   expect(res.statusCode).toStrictEqual(403);
-})
+});
 
 test('Valid token is provided but quiz doesnt exist', () => {
-  const res = requestCreateSession(token, quizId + 1, 3)
+  const res = requestCreateSession(token, quizId + 1, 3);
   const data = JSON.parse(res.body.toString());
 
   expect(data).toStrictEqual({ error: expect.any(String) });
   expect(res.statusCode).toStrictEqual(403);
-})
+});
 
 test('autoStartNum is a number greater than 50', () => {
-  const res = requestCreateSession(token, quizId, 53)
+  const res = requestCreateSession(token, quizId, 53);
   const data = JSON.parse(res.body.toString());
 
   expect(data).toStrictEqual({ error: expect.any(String) });
   expect(res.statusCode).toStrictEqual(400);
-})
+});
 
 test('10 sessions that are not in END state currently exist for this quiz', () => {
   requestCreateSession(token, quizId, 3);
@@ -65,49 +66,47 @@ test('10 sessions that are not in END state currently exist for this quiz', () =
   requestCreateSession(token, quizId, 3);
   requestCreateSession(token, quizId, 3);
 
-  const res = requestCreateSession(token, quizId, 3)
+  const res = requestCreateSession(token, quizId, 3);
   const data = JSON.parse(res.body.toString());
 
   expect(data).toStrictEqual({ error: expect.any(String) });
   expect(res.statusCode).toStrictEqual(400);
-})
+});
 
 test('The quiz does not have any questions in it', () => {
-  const quizId2 = requestCreateQuiz(token, 'valid_name', 'valid_description');
-  
-  const res = requestCreateSession(token, quizId2, 3)
+  const quizId2 = requestCreateQuiz(token, 'valid_name2', 'valid_description2');
+
+  const res = requestCreateSession(token, quizId2, 3);
   const data = JSON.parse(res.body.toString());
 
   expect(data).toStrictEqual({ error: expect.any(String) });
   expect(res.statusCode).toStrictEqual(400);
-})
+});
 
 test('The quiz is in trash', () => {
   requestQuizDelete(token, quizId);
 
-  const res = requestCreateSession(token, quizId, 3)
+  const res = requestCreateSession(token, quizId, 3);
   const data = JSON.parse(res.body.toString());
 
   expect(data).toStrictEqual({ error: expect.any(String) });
   expect(res.statusCode).toStrictEqual(400);
-})
-
-
-test('Correct return object in success case', () => {
-  const res = requestCreateSession(token, quizId, 3)
-  const data = JSON.parse(res.body.toString());
-
-  expect(data).toStrictEqual( { sessionId: expect.any(Number)});
-  expect(res.statusCode).toStrictEqual(200);
 });
 
+test('Correct return object in success case', () => {
+  const res = requestCreateSession(token, quizId, 3);
+  const data = JSON.parse(res.body.toString());
+
+  expect(data).toStrictEqual({ sessionId: expect.any(Number) });
+  expect(res.statusCode).toStrictEqual(200);
+});
 
 // HELPER FUNCTIONS
 const requestCreateSession = (token: string, quizid: number, autoStartNum: number) => {
   return (request('POST', SERVER_URL + `/v1/admin/quiz/${quizid}/session/start`, {
     headers: { token }, json: { autoStartNum: autoStartNum }, timeout: TIMEOUT_MS
-  }))
-}
+  }));
+};
 
 const requestAuthRegister = (email: string, password: string, nameFirst: string, nameLast: string) => {
   const uid = (request('POST', SERVER_URL + '/v1/admin/auth/register', {
@@ -115,7 +114,7 @@ const requestAuthRegister = (email: string, password: string, nameFirst: string,
   }));
   return JSON.parse(uid.body.toString()).token;
 };
-  
+
 const requestCreateQuiz = (token: string, name : string, description : string) => {
   const quiz = (request('POST', SERVER_URL + '/v1/admin/quiz', {
     json: { token, name, description }, timeout: TIMEOUT_MS
@@ -135,11 +134,10 @@ const createQuizQuestion = (token: string, quizid: number, question: string, dur
       }
     }
   });
-}
+};
 
 const requestQuizDelete = (token: string, quizId: number) => {
   return (request('DELETE', SERVER_URL + `/v1/admin/quiz/${quizId}`, {
     qs: { token: token }, timeout: TIMEOUT_MS
   }));
 };
-

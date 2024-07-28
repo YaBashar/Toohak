682417ae1101eb32cb8ea9b2775f9bda0a6e014c
Parampreet Kaur <z5478214@ad.unsw.edu.au>
2024-07-28 14:10:27 +1000

    created route for player join function in server.ts

--- src/server.ts
+++ src/server.ts
@@ -27,7 +27,7 @@ import {
 } from './question';
 
 import {
-  adminGameCreateSession
+  adminGameCreateSession, adminGamePlayerJoin
 } from './game';
 
 // Set up app
@@ -919,6 +919,17 @@ app.post('/v1/admin/quiz/:quizid/session/start', (req: Request, res: Response) =
     }
   }
 });
+
+//adminGamePlayerJoin
+app.post('/v1/player/join', (req: Request, res: Response) => {
+  const { sessionId, name } = req.body;
+  try {
+    const data = adminGamePlayerJoin(sessionId, name);
+    res.json(data);
+  } catch(error) {
+    return res.status(404).json({error: error.message});
+  }
+})
 
 /// ////////////////////////////////////////////////////////////////////////////
 
